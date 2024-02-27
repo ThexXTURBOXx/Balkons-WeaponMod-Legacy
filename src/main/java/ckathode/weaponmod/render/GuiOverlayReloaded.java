c91a16bf374ca8fddb47dc3c96b3d823ec1d8cca
Nico Mexis <nico.mexis@kabelmail.de>
2024-02-27 22:33:47 +0100

    Rework Gui Overlay

--- src/main/java/ckathode/weaponmod/render/GuiOverlayReloaded.java
+++ src/main/java/ckathode/weaponmod/render/GuiOverlayReloaded.java
@@ -2,11 +2,15 @@ package ckathode.weaponmod.render;
 
 import ckathode.weaponmod.item.IItemWeapon;
 import ckathode.weaponmod.item.RangedComponent;
+import net.minecraft.client.MainWindow;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.Gui;
 import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.EnumHand;
+import net.minecraft.util.EnumHandSide;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.client.event.RenderGameOverlayEvent;
@@ -22,42 +26,42 @@ public class GuiOverlayReloaded extends Gui {
 
     @SubscribeEvent
     public void renderGUIOverlay(RenderGameOverlayEvent e) {
-        if (e instanceof RenderGameOverlayEvent.Post || e.getType() != RenderGameOverlayEvent.ElementType.HOTBAR) {
+        if (e instanceof RenderGameOverlayEvent.Post || e.getType() != RenderGameOverlayEvent.ElementType.HOTBAR)
             return;
-        }
+
         EntityPlayer p = mc.player;
-        if (p != null) {
-            ItemStack is = p.getHeldItemMainhand();
-            if (!is.isEmpty() && is.getItem() instanceof IItemWeapon && ((IItemWeapon) is.getItem()).getRangedComponent() != null) {
-                GlStateManager.pushLightingAttrib();
-                RangedComponent rc = ((IItemWeapon) is.getItem()).getRangedComponent();
-                boolean rld = RangedComponent.isReloaded(is);
-                GlStateManager.color4f(1.0f, 1.0f, 1.0f, 1.0f);
-                GlStateManager.disableLighting();
-                float f;
-                int color;
-                if (rld) {
-                    f = 1.0f;
-                    if (p.getActiveItemStack() == is && RangedComponent.isReadyToFire(is)) {
-                        color = 0x60c60000;
-                    } else {
-                        color = 0x60348e00;
-                    }
-                } else if (p.getActiveItemStack() == is) {
-                    f = Math.min(p.getItemInUseMaxCount() / (float) rc.getReloadDuration(is), 1.0f);
-                    color = 0x60eaa800;
-                } else {
-                    f = 0.0f;
-                    color = 0x00000000;
-                }
-                int i;
-                for (i = 0; i < 9 && p.inventory.getStackInSlot(i) != is; ++i) {
-                }
-                int x0 = Minecraft.getInstance().mainWindow.getScaledWidth() / 2 - 88 + i * 20;
-                int y0 = Minecraft.getInstance().mainWindow.getScaledHeight() - 3;
-                drawRect(x0, y0, x0 + 16, y0 - (int) (f * 16.0f), color);
-                GlStateManager.popAttrib();
-            }
+        if (p == null) return;
+        ItemStack is = p.getActiveItemStack();
+        if (is.isEmpty()) return;
+        Item item = is.getItem();
+        if (!(item instanceof IItemWeapon)) return;
+        RangedComponent rc = ((IItemWeapon) item).getRangedComponent();
+        if (rc == null) return;
+
+        EnumHandSide offHandSide = p.getPrimaryHand().opposite();
+        EnumHand hand = p.getActiveHand();
+
+        GlStateManager.pushLightingAttrib();
+        GlStateManager.color4f(1.0f, 1.0f, 1.0f, 1.0f);
+        GlStateManager.disableLighting();
+
+        float f;
+        int color;
+        if (RangedComponent.isReloaded(is)) {
+            f = 1.0f;
+            color = RangedComponent.isReadyToFire(is) ? 0x60c60000 : 0x60348e00;
+        } else {
+            f = Math.min(p.getItemInUseMaxCount() / (float) rc.getReloadDuration(is), 1.0f);
+            color = 0x60eaa800;
         }
+
+        MainWindow window = Minecraft.getInstance().mainWindow;
+        int x0 = window.getScaledWidth() / 2 + (hand == EnumHand.OFF_HAND ?
+                (offHandSide == EnumHandSide.LEFT ? -117 : 101)
+                : -88 + p.inventory.currentItem * 20);
+        int y0 = window.getScaledHeight() - 3;
+
+        drawRect(x0, y0, x0 + 16, y0 - (int) (f * 16.0f), color);
+        GlStateManager.popAttrib();
     }
 }
