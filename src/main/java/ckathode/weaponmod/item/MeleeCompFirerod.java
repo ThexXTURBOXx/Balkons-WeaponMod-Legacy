7d55ecfdf94e7e932f900b62d23041c49ecec2bb
Nico Mexis <nico.mexis@kabelmail.de>
2024-02-28 19:20:26 +0100

    Fix offhand firerod

--- src/main/java/ckathode/weaponmod/item/MeleeCompFirerod.java
+++ src/main/java/ckathode/weaponmod/item/MeleeCompFirerod.java
@@ -7,6 +7,7 @@ import net.minecraft.init.Particles;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.ItemTier;
 import net.minecraft.tags.FluidTags;
+import net.minecraft.util.EnumHandSide;
 import net.minecraft.util.math.MathHelper;
 import net.minecraft.world.World;
 
@@ -16,8 +17,7 @@ public class MeleeCompFirerod extends MeleeComponent {
     }
 
     @Override
-    public boolean hitEntity(ItemStack itemstack, EntityLivingBase entityliving,
-                             EntityLivingBase entityliving1) {
+    public boolean hitEntity(ItemStack itemstack, EntityLivingBase entityliving, EntityLivingBase entityliving1) {
         boolean flag = super.hitEntity(itemstack, entityliving, entityliving1);
         if (flag) {
             entityliving.setFire(12 + weapon.getItemRand().nextInt(3));
@@ -26,23 +26,29 @@ public class MeleeCompFirerod extends MeleeComponent {
     }
 
     @Override
-    public void inventoryTick(ItemStack itemstack, World world, Entity entity, int i,
-                              boolean flag) {
+    public void inventoryTick(ItemStack itemstack, World world, Entity entity, int i, boolean flag) {
         super.inventoryTick(itemstack, world, entity, i, flag);
-        if (entity instanceof EntityPlayer && ((EntityPlayer) entity).inventory.getCurrentItem() == itemstack && !entity.areEyesInFluid(FluidTags.WATER)) {
-            float particleX =
-                    -MathHelper.sin((entity.rotationYaw + 28.0f) * 0.017453292f) * MathHelper.cos(entity.rotationPitch * 0.017453292f) * 1.0f;
-            float particleY = -MathHelper.sin(entity.rotationPitch * 0.017453292f) + 1.6f;
-            float particleZ =
-                    MathHelper.cos((entity.rotationYaw + 28.0f) * 0.017453292f) * MathHelper.cos(entity.rotationPitch * 0.017453292f) * 1.0f;
-            if (weapon.getItemRand().nextInt(5) == 0) {
-                world.addParticle(Particles.FLAME, entity.posX + particleX, entity.posY + particleY,
-                        entity.posZ + particleZ, 0.0, 0.0, 0.0);
-            }
-            if (weapon.getItemRand().nextInt(5) == 0) {
-                world.addParticle(Particles.SMOKE, entity.posX + particleX, entity.posY + particleY,
-                        entity.posZ + particleZ, 0.0, 0.0, 0.0);
-            }
+        if (!(entity instanceof EntityPlayer)) return;
+        EntityPlayer player = (EntityPlayer) entity;
+        if (player.areEyesInFluid(FluidTags.WATER)) return;
+        boolean mainHand = player.getHeldItemMainhand() == itemstack;
+        boolean offHand = player.getHeldItemOffhand() == itemstack;
+        if (!mainHand && !offHand) return;
+
+        float f = 1.0f;
+        float f1 = offHand ^ (player.getPrimaryHand() == EnumHandSide.LEFT) ? -28.0f : 28.0f;
+        float particleX =
+                -MathHelper.sin(((player.rotationYaw + f1) / 180F) * 3.141593F) * MathHelper.cos((player.rotationPitch / 180F) * 3.141593F) * f;
+        float particleY = -MathHelper.sin((player.rotationPitch / 180F) * 3.141593F) + player.getEyeHeight();
+        float particleZ =
+                MathHelper.cos(((player.rotationYaw + f1) / 180F) * 3.141593F) * MathHelper.cos((player.rotationPitch / 180F) * 3.141593F) * f;
+        if (weapon.getItemRand().nextInt(5) == 0) {
+            world.addParticle(Particles.FLAME, player.posX + particleX, player.posY + particleY,
+                    player.posZ + particleZ, 0.0D, 0.0D, 0.0D);
+        }
+        if (weapon.getItemRand().nextInt(5) == 0) {
+            world.addParticle(Particles.SMOKE, player.posX + particleX, player.posY + particleY,
+                    player.posZ + particleZ, 0.0D, 0.0D, 0.0D);
         }
     }
 }
