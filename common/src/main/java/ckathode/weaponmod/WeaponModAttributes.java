cb0f3c329cba3a22515edbb5653522420957ef7b
Nico Mexis <nico.mexis@kabelmail.de>
2024-06-18 09:29:40 +0200

    Fix formatting and attribute names
    Fixes #9

--- common/src/main/java/ckathode/weaponmod/WeaponModAttributes.java
+++ common/src/main/java/ckathode/weaponmod/WeaponModAttributes.java
@@ -10,11 +10,11 @@ public class WeaponModAttributes {
     private static final String PREFIX = "attribute." + MOD_ID;
 
     public static final Attribute IGNORE_ARMOUR_DAMAGE =
-            new RangedAttribute(PREFIX + ".ignoreArmour", 0.0, 0.0, 2048.0).setSyncable(true);
+            new RangedAttribute(PREFIX + ".ignore-armour", 0.0, 0.0, 2048.0).setSyncable(true);
     public static final Attribute WEAPON_KNOCKBACK =
             new RangedAttribute(PREFIX + ".knockback", 0.4, 0.0, 2048.0).setSyncable(true);
     public static final Attribute RELOAD_TIME =
-            new RangedAttribute(PREFIX + ".reloadTime", 0.0, 0.0, 2048.0).setSyncable(true);
+            new RangedAttribute(PREFIX + ".reload-time", 0.0, 0.0, 2048.0).setSyncable(true);
     public static final Attribute WEAPON_REACH =
             new RangedAttribute(PREFIX + ".reach", 0.0, 0.0, 2048.0).setSyncable(true);
 
