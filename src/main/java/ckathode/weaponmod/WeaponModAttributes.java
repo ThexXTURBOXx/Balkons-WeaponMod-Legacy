bb5a75ed9ed3a12f0f63655336a14e1b6cbaeefa
Nico Mexis <nico.mexis@kabelmail.de>
2024-06-18 08:09:44 +0200

    Add Crowdin translations
    Fixes #4

--- src/main/java/ckathode/weaponmod/WeaponModAttributes.java
+++ src/main/java/ckathode/weaponmod/WeaponModAttributes.java
@@ -6,11 +6,11 @@ import net.minecraft.entity.ai.attributes.RangedAttribute;
 
 public class WeaponModAttributes extends SharedMonsterAttributes {
     public static final Attribute IGNORE_ARMOUR_DAMAGE =
-            new RangedAttribute(null, BalkonsWeaponMod.MOD_ID + ".ignoreArmour", 0.0, 0.0, 2048.0).setShouldWatch(true);
+            new RangedAttribute(null, BalkonsWeaponMod.MOD_ID + ".ignore-armour", 0.0, 0.0, 2048.0).setShouldWatch(true);
     public static final Attribute WEAPON_KNOCKBACK =
             new RangedAttribute(null, BalkonsWeaponMod.MOD_ID + ".knockback", 0.4, 0.0, 2048.0).setShouldWatch(true);
     public static final Attribute RELOAD_TIME =
-            new RangedAttribute(null, BalkonsWeaponMod.MOD_ID + ".reloadTime", 0.0, 0.0, 2048.0).setShouldWatch(true);
+            new RangedAttribute(null, BalkonsWeaponMod.MOD_ID + ".reload-time", 0.0, 0.0, 2048.0).setShouldWatch(true);
     public static final Attribute WEAPON_REACH =
             new RangedAttribute(null, BalkonsWeaponMod.MOD_ID + ".reach", 0.0, 0.0, 2048.0).setShouldWatch(true);
 
