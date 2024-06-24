9f69798a92918c093d9e33f7e1a7948d8082f8c5
Nico Mexis <nico.mexis@kabelmail.de>
2024-06-24 23:08:33 +0200

    API fixes

--- common/src/main/java/ckathode/weaponmod/entity/projectile/EntityMaterialProjectile.java
+++ common/src/main/java/ckathode/weaponmod/entity/projectile/EntityMaterialProjectile.java
@@ -79,8 +79,8 @@ public abstract class EntityMaterialProjectile<T extends EntityMaterialProjectil
     }
 
     public void setThrownItemStack(@NotNull ItemStack itemstack) {
-        updateWeaponMaterial();
         entityData.set(WEAPON_ITEM, itemstack);
+        updateWeaponMaterial();
     }
 
     @NotNull
@@ -99,11 +99,10 @@ public abstract class EntityMaterialProjectile<T extends EntityMaterialProjectil
 
     protected void updateWeaponMaterial() {
         ItemStack thrownItem = getWeapon();
-        if (thrownItem != null && thrownItem.getItem() instanceof IItemWeapon && ((IItemWeapon) thrownItem.getItem()).getMeleeComponent() != null) {
+        if (thrownItem != null && thrownItem.getItem() instanceof IItemWeapon weapon && weapon.getMeleeComponent() != null) {
             int material = MaterialRegistry.getMaterialID(thrownItem);
             if (material < 0) {
-                material =
-                        MaterialRegistry.getOrdinal(((IItemWeapon) thrownItem.getItem()).getMeleeComponent().weaponMaterial);
+                material = MaterialRegistry.getOrdinal(weapon.getMeleeComponent().weaponMaterial);
             }
             entityData.set(WEAPON_MATERIAL, (byte) (material & 0xFF));
         }
