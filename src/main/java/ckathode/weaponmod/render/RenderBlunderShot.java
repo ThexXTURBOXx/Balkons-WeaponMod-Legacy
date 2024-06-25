d659f10b52ef6f76dd0497e5d18e18c7812ca64a
Nico Mexis <nico.mexis@kabelmail.de>
2024-06-25 22:21:17 +0200

    Remove "shooting stars"
    Unnecessary cosmetic thing anyway...
    Fixes #30

--- src/main/java/ckathode/weaponmod/render/RenderBlunderShot.java
+++ src/main/java/ckathode/weaponmod/render/RenderBlunderShot.java
@@ -3,12 +3,10 @@ package ckathode.weaponmod.render;
 import ckathode.weaponmod.WeaponModResources;
 import ckathode.weaponmod.entity.projectile.EntityBlunderShot;
 import com.mojang.blaze3d.matrix.MatrixStack;
-import com.mojang.blaze3d.platform.GlStateManager;
 import com.mojang.blaze3d.vertex.IVertexBuilder;
 import javax.annotation.Nonnull;
 import javax.annotation.ParametersAreNonnullByDefault;
 import net.minecraft.client.renderer.IRenderTypeBuffer;
-import net.minecraft.client.renderer.Matrix4f;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.Vector3f;
 import net.minecraft.client.renderer.entity.EntityRendererManager;
@@ -45,7 +43,7 @@ public class RenderBlunderShot extends WMRenderer<EntityBlunderShot> {
             drawVertex(last, builder, -1.0f, 1.0f, 0.0f, 0.0f, 0.3125f, 0.0f, 0.0f, 0.05625f, lm);
         }
         ms.pop();
-        ms.push();
+        /*ms.push();
         GlStateManager.disableTexture();
         GlStateManager.disableCull();
         GlStateManager.lineWidth(1.0f);
@@ -63,7 +61,7 @@ public class RenderBlunderShot extends WMRenderer<EntityBlunderShot> {
                 .endVertex();
         GlStateManager.enableTexture();
         GlStateManager.enableCull();
-        ms.pop();
+        ms.pop();*/
         super.render(entityblundershot, f, f1, ms, bufs, lm);
     }
 
