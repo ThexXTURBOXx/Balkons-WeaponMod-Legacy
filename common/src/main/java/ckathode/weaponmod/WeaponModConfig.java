a7c81f01062ba1d2b4aefa832f09bbf202ea18b2
Nico Mexis <nico.mexis@kabelmail.de>
2024-06-25 10:07:03 +0200

    Server fixes

--- common/src/main/java/ckathode/weaponmod/WeaponModConfig.java
+++ common/src/main/java/ckathode/weaponmod/WeaponModConfig.java
@@ -10,13 +10,8 @@ import me.shedaniel.autoconfig.AutoConfig;
 import me.shedaniel.autoconfig.ConfigData;
 import me.shedaniel.autoconfig.annotation.Config;
 import me.shedaniel.autoconfig.annotation.ConfigEntry.Category;
-import me.shedaniel.autoconfig.gui.registry.GuiRegistry;
 import me.shedaniel.autoconfig.serializer.JanksonConfigSerializer;
-import me.shedaniel.autoconfig.util.Utils;
 import me.shedaniel.cloth.clothconfig.shadowed.blue.endless.jankson.Comment;
-import me.shedaniel.clothconfig2.api.AbstractConfigListEntry;
-import me.shedaniel.clothconfig2.api.ConfigEntryBuilder;
-import net.minecraft.network.chat.TranslatableComponent;
 
 import static ckathode.weaponmod.BalkonsWeaponMod.MOD_ID;
 
@@ -110,45 +105,18 @@ public class WeaponModConfig implements ConfigData {
         return AutoConfig.getConfigHolder(WeaponModConfig.class).getConfig();
     }
 
-    @SuppressWarnings({"rawtypes"})
     public static void init() {
         AutoConfig.register(WeaponModConfig.class, JanksonConfigSerializer::new);
-
-        GuiRegistry registry = AutoConfig.getGuiRegistry(WeaponModConfig.class);
-        registry.registerAnnotationProvider((i, f, c, d, r) -> {
-            Map<String, Boolean> map = Utils.getUnsafely(f, c);
-            Map<String, Boolean> defaults = Utils.getUnsafely(f, d);
-            return map.entrySet().stream().sorted(Map.Entry.comparingByKey())
-                    .map(e -> ConfigEntryBuilder.create()
-                            .startBooleanToggle(new TranslatableComponent(i + "." + e.getKey()), e.getValue())
-                            .setDefaultValue(() -> defaults.get(e.getKey()))
-                            .setSaveConsumer(s -> map.put(e.getKey(), s))
-                            .build())
-                    .map(e -> (AbstractConfigListEntry) e)
-                    .toList();
-        }, StringBooleanMap.class);
-        registry.registerAnnotationProvider((i, f, c, d, r) -> {
-            Map<String, Integer> map = Utils.getUnsafely(f, c);
-            Map<String, Integer> defaults = Utils.getUnsafely(f, d);
-            return map.entrySet().stream().sorted(Map.Entry.comparingByKey())
-                    .map(e -> ConfigEntryBuilder.create()
-                            .startIntField(new TranslatableComponent(i + "." + e.getKey()), e.getValue())
-                            .setDefaultValue(() -> defaults.get(e.getKey()))
-                            .setSaveConsumer(s -> map.put(e.getKey(), s))
-                            .build())
-                    .map(e -> (AbstractConfigListEntry) e)
-                    .toList();
-        }, StringIntMap.class);
     }
 
     @Retention(RetentionPolicy.RUNTIME)
     @Target(ElementType.FIELD)
-    private @interface StringBooleanMap {
+    public @interface StringBooleanMap {
     }
 
     @Retention(RetentionPolicy.RUNTIME)
     @Target(ElementType.FIELD)
-    private @interface StringIntMap {
+    public @interface StringIntMap {
     }
 
 }
--- common/src/main/java/ckathode/weaponmod/network/MsgExplosion.java
+++ common/src/main/java/ckathode/weaponmod/network/MsgExplosion.java
@@ -1,14 +1,13 @@
 package ckathode.weaponmod.network;
 
 import ckathode.weaponmod.AdvancedExplosion;
+import dev.architectury.networking.NetworkManager;
 import io.netty.buffer.ByteBuf;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Supplier;
-import dev.architectury.networking.NetworkManager;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
-import net.minecraft.client.Minecraft;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.Explosion;
 import net.minecraft.world.level.Level;
@@ -77,7 +76,7 @@ public class MsgExplosion implements WMMessage<MsgExplosion> {
     @Environment(EnvType.CLIENT)
     @Override
     public void handleClientSide(MsgExplosion msg, Supplier<NetworkManager.PacketContext> ctx) {
-        Level world = Minecraft.getInstance().level;
+        Level world = ctx.get().getPlayer().getLevel();
         AdvancedExplosion expl = new AdvancedExplosion(world, null, x, y, z, size,
                 false, Explosion.BlockInteraction.DESTROY);
         expl.setAffectedBlockPositions(blocks);
