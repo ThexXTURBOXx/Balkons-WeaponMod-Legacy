19c7771833d506a63bfdf98e83e10e1bfefa11e7
Nico Mexis <nico.mexis@kabelmail.de>
2024-06-25 10:37:05 +0200

    API fixes

--- src/main/java/ckathode/weaponmod/entity/projectile/EntityMaterialProjectile.java
+++ src/main/java/ckathode/weaponmod/entity/projectile/EntityMaterialProjectile.java
@@ -57,8 +57,8 @@ public abstract class EntityMaterialProjectile extends EntityProjectile {
 
     public void setThrownItemStack(@Nullable ItemStack itemstack) {
         thrownItem = itemstack;
-        updateWeaponMaterial();
         dataWatcher.updateObject(WEAPON_ITEM, itemstack);
+        updateWeaponMaterial();
     }
 
     @Nullable
