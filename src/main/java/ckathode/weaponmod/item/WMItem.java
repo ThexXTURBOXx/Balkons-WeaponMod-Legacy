46dd75796625f9cba016a92d1a64eb7e4b8cb463
Nico Mexis <nico.mexis@kabelmail.de>
2024-06-29 16:13:37 +0200

    Damage weapons when blocking damage
    Fixes #32

--- src/main/java/ckathode/weaponmod/item/WMItem.java
+++ src/main/java/ckathode/weaponmod/item/WMItem.java
@@ -2,11 +2,15 @@ package ckathode.weaponmod.item;
 
 import ckathode.weaponmod.BalkonsWeaponMod;
 import java.util.UUID;
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.ResourceLocation;
+import org.jetbrains.annotations.Nullable;
 
 public class WMItem extends Item {
+
     public WMItem(String id) {
         this(id, new Properties());
     }
@@ -23,4 +27,13 @@ public class WMItem extends Item {
     public static UUID getAttackSpeedModifierUUID() {
         return Item.ATTACK_SPEED_MODIFIER;
     }
+
+    @Override
+    public boolean isShield(ItemStack stack, @Nullable LivingEntity entity) {
+        if (entity != null && entity.getActiveItemStack() == stack && stack.getItem() instanceof WMItem &&
+            entity.isActiveItemStackBlocking())
+            return true;
+        return super.isShield(stack, entity);
+    }
+
 }
