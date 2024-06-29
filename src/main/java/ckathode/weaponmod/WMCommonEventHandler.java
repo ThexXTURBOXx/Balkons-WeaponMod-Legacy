f06865b835dd368e5a7a7ce532824bec9507c8be
Nico Mexis <nico.mexis@kabelmail.de>
2024-06-29 15:43:59 +0200

    Damage weapons when blocking damage
    Fixes #32

--- src/main/java/ckathode/weaponmod/WMCommonEventHandler.java
+++ src/main/java/ckathode/weaponmod/WMCommonEventHandler.java
@@ -1,8 +1,15 @@
 package ckathode.weaponmod;
 
+import ckathode.weaponmod.item.ItemMelee;
+import ckathode.weaponmod.item.ItemShooter;
+import ckathode.weaponmod.item.WMItem;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.math.MathHelper;
 import net.minecraftforge.event.entity.EntityEvent;
+import net.minecraftforge.event.entity.living.LivingAttackEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 public class WMCommonEventHandler {
@@ -12,5 +19,20 @@ public class WMCommonEventHandler {
         if (entity instanceof EntityPlayer) {
             PlayerWeaponData.initPlayerWeaponData((EntityPlayer) entity);
         }
+    }
+
+    @SubscribeEvent
+    public void onLivingAttack(LivingAttackEvent event) {
+        if (event.getAmount() < 3.0f) return;
+        EntityLivingBase entity = event.getEntityLiving();
+        if (!(entity instanceof EntityPlayer)) return;
+        EntityPlayer player = (EntityPlayer) entity;
+        ItemStack stack = player.getActiveItemStack();
+        if (stack.isEmpty()) return;
+        if (!player.isActiveItemStackBlocking()) return;
+        if (!(stack.getItem() instanceof ItemMelee) && !(stack.getItem() instanceof ItemShooter) &&
+            !(stack.getItem() instanceof WMItem)) return;
+        int i = 1 + MathHelper.floor(event.getAmount());
+        stack.damageItem(i, player);
     }
 }
