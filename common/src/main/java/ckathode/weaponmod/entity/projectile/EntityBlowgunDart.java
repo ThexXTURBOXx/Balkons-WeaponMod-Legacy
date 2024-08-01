8edcf87e4758e185539f2ce8a7a0857db9f18da7
Nico Mexis <nico.mexis@kabelmail.de>
2024-08-01 11:05:58 +0200

    Make Darts more resistant to bugs
    Fixes #59

--- common/src/main/java/ckathode/weaponmod/entity/projectile/EntityBlowgunDart.java
+++ common/src/main/java/ckathode/weaponmod/entity/projectile/EntityBlowgunDart.java
@@ -86,11 +86,14 @@ public class EntityBlowgunDart extends EntityProjectile<EntityBlowgunDart> {
     }
 
     public byte getDartEffectId() {
-        return entityData.get(DART_EFFECT_TYPE);
+        byte effectId = entityData.get(DART_EFFECT_TYPE);
+        if (effectId < 0) effectId = 0;
+        return effectId;
     }
 
     public float[] getDartColor() {
-        return DART_COLORS[getDartEffectId()];
+        int effectId = getDartEffectId();
+        return DART_COLORS[effectId >= DART_COLORS.length ? 0 : effectId];
     }
 
     @Override
@@ -98,7 +101,7 @@ public class EntityBlowgunDart extends EntityProjectile<EntityBlowgunDart> {
         DamageSource damagesource = WeaponDamageSource.causeProjectileWeaponDamage(this, getDamagingEntity());
         if (entity.hurt(damagesource, 1.0f + extraDamage)) {
             if (entity instanceof LivingEntity) {
-                ((LivingEntity) entity).addEffect(new MobEffectInstance(DartType.dartTypes[getDartEffectId()].potionEffect));
+                ((LivingEntity) entity).addEffect(new MobEffectInstance(getDartEffectType().potionEffect));
             }
             applyEntityHitEffects(entity);
             playHitSound();
