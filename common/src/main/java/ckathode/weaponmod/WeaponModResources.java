744d3cc2c748dec686b4042f9ac8e73935a2b2bd
Nico Mexis <nico.mexis@kabelmail.de>
2024-08-01 09:49:24 +0200

    Better Mod-ID handling

--- common/src/main/java/ckathode/weaponmod/WeaponModResources.java
+++ common/src/main/java/ckathode/weaponmod/WeaponModResources.java
@@ -4,37 +4,39 @@ import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.minecraft.resources.ResourceLocation;
 
+import static ckathode.weaponmod.BalkonsWeaponMod.MOD_ID;
+
 @Environment(EnvType.CLIENT)
 public final class WeaponModResources {
     public static final class Entity {
         public static final ResourceLocation DART = new ResourceLocation(
-                BalkonsWeaponMod.MOD_ID, "textures/entity/blowgun_dart.png");
+                MOD_ID, "textures/entity/blowgun_dart.png");
         public static final ResourceLocation BOOMERANG = new ResourceLocation(
-                BalkonsWeaponMod.MOD_ID, "textures/entity/boomerang.png");
+                MOD_ID, "textures/entity/boomerang.png");
         public static final ResourceLocation CANNON = new ResourceLocation(
-                BalkonsWeaponMod.MOD_ID, "textures/entity/cannon.png");
+                MOD_ID, "textures/entity/cannon.png");
         public static final ResourceLocation CANNONBALL = new ResourceLocation(
-                BalkonsWeaponMod.MOD_ID, "textures/entity/cannon_ball.png");
+                MOD_ID, "textures/entity/cannon_ball.png");
         public static final ResourceLocation BOLT = new ResourceLocation(
-                BalkonsWeaponMod.MOD_ID, "textures/entity/crossbow_bolt.png");
+                MOD_ID, "textures/entity/crossbow_bolt.png");
         public static final ResourceLocation DUMMY = new ResourceLocation(
-                BalkonsWeaponMod.MOD_ID, "textures/entity/dummy.png");
+                MOD_ID, "textures/entity/dummy.png");
         public static final ResourceLocation DYNAMITE = new ResourceLocation(
-                BalkonsWeaponMod.MOD_ID, "textures/entity/dynamite.png");
+                MOD_ID, "textures/entity/dynamite.png");
         public static final ResourceLocation FLAIL = new ResourceLocation(
-                BalkonsWeaponMod.MOD_ID, "textures/entity/flail.png");
+                MOD_ID, "textures/entity/flail.png");
         public static final ResourceLocation JAVELIN = new ResourceLocation(
-                BalkonsWeaponMod.MOD_ID, "textures/entity/spear.png");
+                MOD_ID, "textures/entity/spear.png");
         public static final ResourceLocation KNIFE = new ResourceLocation(
-                BalkonsWeaponMod.MOD_ID, "textures/entity/knife.png");
+                MOD_ID, "textures/entity/knife.png");
         public static final ResourceLocation BULLET = new ResourceLocation(
-                BalkonsWeaponMod.MOD_ID, "textures/entity/musket_bullet.png");
+                MOD_ID, "textures/entity/musket_bullet.png");
         public static final ResourceLocation SPEAR = new ResourceLocation(
-                BalkonsWeaponMod.MOD_ID, "textures/entity/spear.png");
+                MOD_ID, "textures/entity/spear.png");
     }
 
     public static final class Gui {
         public static final ResourceLocation OVERLAY = new ResourceLocation(
-                BalkonsWeaponMod.MOD_ID, "textures/gui/overlay.png");
+                MOD_ID, "textures/gui/overlay.png");
     }
 }
