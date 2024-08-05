8f423ba06f75d004e0e3a1d565e708afe0ee5afc
Nico Mexis <nico.mexis@kabelmail.de>
2024-08-05 23:46:16 +0200

    Fix custom battleaxe attributes
    Fixes #61

--- src/main/java/ckathode/weaponmod/item/MeleeCompBattleaxe.java
+++ src/main/java/ckathode/weaponmod/item/MeleeCompBattleaxe.java
@@ -14,14 +14,9 @@ import net.minecraft.item.IItemTier;
 import net.minecraft.item.ItemStack;
 
 public class MeleeCompBattleaxe extends MeleeComponent {
-    public static final float[] DEFAULT_IGNORES = new float[]{1, 1, 1, 1, 1};
-    public final float ignoreArmourAmount;
 
     public MeleeCompBattleaxe(IItemTier itemTier) {
         super(MeleeSpecs.BATTLEAXE, itemTier);
-        int ordinal = MaterialRegistry.getOrdinal(itemTier);
-        ignoreArmourAmount = ordinal >= 0 && ordinal < DEFAULT_IGNORES.length
-                ? DEFAULT_IGNORES[ordinal] : 0;
     }
 
     @Override
@@ -33,7 +28,7 @@ public class MeleeCompBattleaxe extends MeleeComponent {
             double mz = entity.motionZ;
             int prevhurtres = living.hurtResistantTime;
             int prevhurt = living.hurtTime;
-            living.attackEntityFrom(new DamageSourceAxe(), ignoreArmourAmount);
+            living.attackEntityFrom(new DamageSourceAxe(), getIgnoreArmorAmount(weaponMaterial));
             entity.motionX = mx;
             entity.motionY = my;
             entity.motionZ = mz;
@@ -57,9 +52,15 @@ public class MeleeCompBattleaxe extends MeleeComponent {
     @Override
     public void addItemAttributeModifiers(Multimap<String, AttributeModifier> multimap) {
         super.addItemAttributeModifiers(multimap);
-        multimap.put(WeaponModAttributes.IGNORE_ARMOUR_DAMAGE.getName(),
-                new AttributeModifier(IItemWeapon.IGNORE_ARMOUR_MODIFIER,
-                        "Weapon ignore armour modifier", ignoreArmourAmount, 0));
+        if (getIgnoreArmorAmount(weaponMaterial) != 0.0f) {
+            multimap.put(WeaponModAttributes.IGNORE_ARMOUR_DAMAGE.getName(),
+                    new AttributeModifier(IItemWeapon.IGNORE_ARMOUR_MODIFIER,
+                            "Weapon ignore armour modifier", getIgnoreArmorAmount(weaponMaterial), 0));
+        }
+    }
+
+    public float getIgnoreArmorAmount(IItemTier itemTier) {
+        return 1.0f;
     }
 
 }
