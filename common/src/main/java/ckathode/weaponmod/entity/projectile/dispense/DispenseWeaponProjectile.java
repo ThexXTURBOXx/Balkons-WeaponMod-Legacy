34a3077ed87ca6ef1352ddf66023752b2bf6c3c3
Nico Mexis <nico.mexis@kabelmail.de>
2024-08-06 00:09:09 +0200

    Better parameter names and reformat

--- common/src/main/java/ckathode/weaponmod/entity/projectile/dispense/DispenseWeaponProjectile.java
+++ common/src/main/java/ckathode/weaponmod/entity/projectile/dispense/DispenseWeaponProjectile.java
@@ -1,6 +1,5 @@
 package ckathode.weaponmod.entity.projectile.dispense;
 
-import org.jetbrains.annotations.NotNull;
 import net.minecraft.core.BlockSource;
 import net.minecraft.core.Direction;
 import net.minecraft.core.Position;
@@ -9,21 +8,22 @@ import net.minecraft.world.entity.projectile.Projectile;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.DispenserBlock;
+import org.jetbrains.annotations.NotNull;
 
 public abstract class DispenseWeaponProjectile extends AbstractProjectileDispenseBehavior {
 
     @NotNull
     @Override
-    public ItemStack execute(BlockSource blocksource, @NotNull ItemStack itemstack) {
-        Level world = blocksource.getLevel();
-        Position pos = DispenserBlock.getDispensePosition(blocksource);
-        Direction face = blocksource.getBlockState().getValue(DispenserBlock.FACING);
-        Projectile projectile = getProjectile(world, pos, itemstack);
+    public ItemStack execute(BlockSource blockSource, @NotNull ItemStack stack) {
+        Level world = blockSource.getLevel();
+        Position pos = DispenserBlock.getDispensePosition(blockSource);
+        Direction face = blockSource.getBlockState().getValue(DispenserBlock.FACING);
+        Projectile projectile = getProjectile(world, pos, stack);
         projectile.shoot(face.getStepX(), face.getStepY() + getYVel(), face.getStepZ(), getPower(),
                 getUncertainty());
         world.addFreshEntity(projectile);
-        itemstack.shrink(1);
-        return itemstack;
+        stack.shrink(1);
+        return stack;
     }
 
     public double getYVel() {
