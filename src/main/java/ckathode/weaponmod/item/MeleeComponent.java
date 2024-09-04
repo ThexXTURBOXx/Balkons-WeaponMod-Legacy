ThexXTURBOXx/Balkons-WeaponMod-Legacy

commit dc09b079978eb9f944322d89583f99bde9de404c

Balancing fixes
Fixes #51

src/main/java/ckathode/weaponmod/item/MeleeComponent.java
@@ -198,15 +198,15 @@ public class MeleeComponent extends AbstractWeaponComponent {
     }
 
     public enum MeleeSpecs {
-        SPEAR(0, 1.0f, 2.0f, 1.0f, 1.0f, 0.2f, 1, 2, 2.7f),
-        HALBERD(0, 1.0f, 3.0f, 1.0f, 1.5f, 0.6f, 1, 2, 3.2f),
-        BATTLEAXE(0, 1.0f, 2.0f, 1.0f, 1.5f, 0.5f, 1, 2, 3.0f),
-        WARHAMMER(0, 1.0f, 3.0f, 1.0f, 1.0f, 0.7f, 1, 2, 3.0f),
-        KNIFE(0, 0.5f, 2.0f, 1.0f, 1.5f, 0.2f, 1, 2, 2.0f),
-        KATANA(0, 1.0f, 0.0f, 1.0f, 1.0f, 0.0f, 1, 2, 0.2f),
-        FIREROD(1, 0.0f, 0.0f, 0.0f, 1.0f, 0.4f, 2, 0, 0.0f),
-        BOOMERANG(0, 0.5f, 1.0f, 1.0f, 1.0f, 0.4f, 1, 1, 2.0f),
-        NONE(0, 1.0f, 1.0f, 0.0f, 1.0f, 0.4f, 0, 0, 0.0f);
+        SPEAR(0, 1.0f, 3.0f, 1.0f, 1.0f, 0.2f, 1, 2, 2.7f),
+        HALBERD(0, 1.0f, 4.0f, 1.0f, 1.5f, 0.6f, 1, 2, 3.2f),
+        BATTLEAXE(0, 1.0f, 3.0f, 1.0f, 1.5f, 0.5f, 1, 2, 3.0f),
+        WARHAMMER(0, 1.0f, 4.0f, 1.0f, 1.0f, 0.7f, 1, 2, 3.0f),
+        KNIFE(0, 0.5f, 3.0f, 1.0f, 1.5f, 0.2f, 1, 2, 2.0f),
+        KATANA(0, 1.0f, 1.0f, 1.0f, 1.0f, 0.0f, 1, 2, 0.2f),
+        FIREROD(1, 0.0f, 1.0f, 0.0f, 1.0f, 0.4f, 2, 0, 0.0f),
+        BOOMERANG(0, 0.5f, 2.0f, 1.0f, 1.0f, 0.4f, 1, 1, 2.0f),
+        NONE(0, 0.0f, 1.0f, 0.0f, 1.0f, 0.4f, 0, 0, 0.0f);
 
         public final int durabilityBase;
         public final float durabilityMult;

src/main/java/ckathode/weaponmod/entity/projectile/EntityBoomerang.java
@@ -123,7 +123,7 @@ public class EntityBoomerang extends EntityMaterialProjectile {
         }
         DamageSource damagesource = WeaponDamageSource.causeProjectileWeaponDamage(this, getDamagingEntity());
         float damage =
-                ((IItemWeapon) thrownItem.getItem()).getMeleeComponent().getEntityDamage() + 3.0f + extraDamage;
+                ((IItemWeapon) thrownItem.getItem()).getMeleeComponent().getEntityDamage() + 2.0f + extraDamage;
         damage += getMeleeHitDamage(entity);
         if (getIsCritical()) {
             damage += 2.0f;

src/main/java/ckathode/weaponmod/entity/projectile/EntityKnife.java
@@ -71,7 +71,7 @@ public class EntityKnife extends EntityMaterialProjectile {
         DamageSource damagesource = WeaponDamageSource.causeProjectileWeaponDamage(this, getDamagingEntity());
         Item item = thrownItem.getItem();
         if (item instanceof IItemWeapon && entity.attackEntityFrom(damagesource,
-                ((IItemWeapon) item).getMeleeComponent().getEntityDamage() + 1.0f + getMeleeHitDamage(entity))) {
+                ((IItemWeapon) item).getMeleeComponent().getEntityDamage() + getMeleeHitDamage(entity))) {
             applyEntityHitEffects(entity);
             if (thrownItem.getItemDamage() + 2 >= thrownItem.getMaxDamage()) {
                 thrownItem.splitStack(1);

src/main/java/ckathode/weaponmod/entity/projectile/EntitySpear.java
@@ -50,7 +50,7 @@ public class EntitySpear extends EntityMaterialProjectile {
         DamageSource damagesource = WeaponDamageSource.causeProjectileWeaponDamage(this, getDamagingEntity());
         Item item = thrownItem.getItem();
         if (item instanceof IItemWeapon && entity.attackEntityFrom(damagesource,
-                ((IItemWeapon) item).getMeleeComponent().getEntityDamage() + 1.0f + getMeleeHitDamage(entity))) {
+                ((IItemWeapon) item).getMeleeComponent().getEntityDamage() + getMeleeHitDamage(entity))) {
             applyEntityHitEffects(entity);
             playHitSound();
             if (thrownItem.getItemDamage() + 1 >= thrownItem.getMaxDamage()) {

commit af38c10afa401d2ff2a5bab63f2b08ffee5e4389

Fix Flail not taking damage
I broke this by accident in dc09b079978eb9f944322d89583f99bde9de404c
Fixes #63

src/main/java/ckathode/weaponmod/item/MeleeComponent.java
@@ -206,7 +206,7 @@ public class MeleeComponent extends AbstractWeaponComponent {
         KATANA(0, 1.0f, 1.0f, 1.0f, 1.0f, 0.0f, 1, 2, 0.2f),
         FIREROD(1, 0.0f, 1.0f, 0.0f, 1.0f, 0.4f, 2, 0, 0.0f),
         BOOMERANG(0, 0.5f, 2.0f, 1.0f, 1.0f, 0.4f, 1, 1, 2.0f),
-        NONE(0, 0.0f, 1.0f, 0.0f, 1.0f, 0.4f, 0, 0, 0.0f);
+        NONE(0, 1.0f, 1.0f, 0.0f, 1.0f, 0.4f, 0, 0, 0.0f);
 
         public final int durabilityBase;
         public final float durabilityMult;