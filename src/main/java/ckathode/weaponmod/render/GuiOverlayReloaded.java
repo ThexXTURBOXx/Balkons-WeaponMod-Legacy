2b6c88c3be3d52dacb46e35b54f4c55de3ffb6bd
Nico Mexis <nico.mexis@kabelmail.de>
2024-10-20 20:15:19 +0200

    Fix reload overlay

--- src/main/java/ckathode/weaponmod/render/GuiOverlayReloaded.java
+++ src/main/java/ckathode/weaponmod/render/GuiOverlayReloaded.java
@@ -29,15 +29,12 @@ public class GuiOverlayReloaded extends AbstractGui {
         Minecraft mc = Minecraft.getInstance();
         PlayerEntity p = mc.player;
         if (p == null) return;
-        if (!p.isHandActive()) return;
 
         renderForHand(Hand.MAIN_HAND, p);
         renderForHand(Hand.OFF_HAND, p);
     }
 
     private void renderForHand(Hand hand, PlayerEntity p) {
-        if (p.getActiveHand() != hand) return;
-
         Minecraft mc = Minecraft.getInstance();
         int currentItem = p.inventory.currentItem;
 
@@ -56,13 +53,15 @@ public class GuiOverlayReloaded extends AbstractGui {
         float f = 0;
         int offset = 0;
         if (rc != null) {
-            if (RangedComponent.isReloaded(is)) {
-                f = 1.0f;
-                offset = RangedComponent.isReadyToFire(is) ? 48 : 24;
-            } else {
-                f = MathHelper.clamp(p.getItemInUseMaxCount() / (float) rc.getReloadDuration(is), 0, 1);
+            if (p.getActiveHand() == hand) {
+                if (RangedComponent.isReloaded(is)) {
+                    f = 1.0f;
+                    offset = RangedComponent.isReadyToFire(is) ? 48 : 24;
+                } else {
+                    f = MathHelper.clamp(p.getItemInUseMaxCount() / (float) rc.getReloadDuration(is), 0, 1);
+                }
+                set = true;
             }
-            set = true;
         } else if (mec != null) {
             if (mec.shouldRenderCooldown()) {
                 f = MathHelper.clamp(mec.getCooldown(), 0, 1);
