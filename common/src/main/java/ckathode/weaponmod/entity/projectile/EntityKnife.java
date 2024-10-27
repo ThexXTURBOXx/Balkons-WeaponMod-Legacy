77a576b11396ba1572ec5def26932ab95e79f178
Nico Mexis <nico.mexis@kabelmail.de>
2024-10-27 22:35:38 +0100

    Prevent overflows...
    ... which should never happen anyway

--- common/src/main/java/ckathode/weaponmod/entity/projectile/EntityKnife.java
+++ common/src/main/java/ckathode/weaponmod/entity/projectile/EntityKnife.java
@@ -73,6 +73,7 @@ public class EntityKnife extends EntityMaterialProjectile<EntityKnife> {
             return;
         }
         setXRot(getXRot() - 70.0f);
+        if (getXRot() <= -360) setXRot(getXRot() + 360.0f);
         if (soundTimer >= 3) {
             if (!isInWater()) {
                 playSound(SoundEvents.ARROW_SHOOT, 0.6f,
