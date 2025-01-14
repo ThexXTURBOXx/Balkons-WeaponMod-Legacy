0a5352ce7b38c57d225e9687fb52ab116d06bed1
Nico Mexis <nico.mexis@kabelmail.de>
2025-01-14 18:47:43 +0100

    Update shadow plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
     java
     id("gg.essential.loom") version "1.6.9999-fg1"
     id("dev.architectury.architectury-pack200") version "0.1.3"
-    id("com.github.johnrengelman.shadow") version "8.1.1"
+    id("com.gradleup.shadow") version "8.3.5"
 }
 
 val minecraft_version: String by project
