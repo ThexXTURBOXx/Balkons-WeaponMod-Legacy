3d8ca0f5f47af1c5be0f802559095fb818adac24
Nico Mexis <nico.mexis@kabelmail.de>
2025-01-14 18:48:23 +0100

    Remove global thrownItem cache variable

--- src/main/java/ckathode/weaponmod/entity/projectile/EntitySpear.java
+++ src/main/java/ckathode/weaponmod/entity/projectile/EntitySpear.java
@@ -48,6 +48,8 @@ public class EntitySpear extends EntityMaterialProjectile {
             return;
         }
         DamageSource damagesource = WeaponDamageSource.causeProjectileWeaponDamage(this, getDamagingEntity());
+        ItemStack thrownItem = getWeapon();
+        if (thrownItem == null) return;
         Item item = thrownItem.getItem();
         if (item instanceof IItemWeapon && entity.attackEntityFrom(damagesource,
                 ((IItemWeapon) item).getMeleeComponent().getEntityDamage() + getMeleeHitDamage(entity))) {
