3b589480ae7dd7763b193840022d9c112eaac547
Nico Mexis <nico.mexis@kabelmail.de>
2025-02-08 16:56:39 +0100

    Indicate bayonet durability
    Fixes #86

--- common/src/main/java/ckathode/weaponmod/item/ItemMusket.java
+++ common/src/main/java/ckathode/weaponmod/item/ItemMusket.java
@@ -5,9 +5,11 @@ import ckathode.weaponmod.ReloadHelper;
 import ckathode.weaponmod.WMItemBuilder;
 import ckathode.weaponmod.WMRegistries;
 import com.mojang.serialization.Codec;
+import java.util.List;
 import java.util.Objects;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.component.DataComponentType;
+import net.minecraft.network.chat.Component;
 import net.minecraft.network.codec.ByteBufCodecs;
 import net.minecraft.stats.Stats;
 import net.minecraft.tags.BlockTags;
@@ -17,6 +19,7 @@ import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Tiers;
+import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockState;
@@ -72,6 +75,19 @@ public class ItemMusket extends ItemShooter {
 
     public boolean hasBayonet() {
         return bayonetItem != null;
+    }
+
+    @Override
+    public void appendHoverText(ItemStack stack, TooltipContext context, List<Component> tooltipComponents,
+                                TooltipFlag tooltipFlag) {
+        super.appendHoverText(stack, context, tooltipComponents, tooltipFlag);
+        if (hasBayonet() && stack.has(BAYONET_DAMAGE_TYPE)) {
+            short dmg = Objects.requireNonNull(stack.get(BAYONET_DAMAGE_TYPE));
+            if (dmg != 0) {
+                tooltipComponents.add(Component.translatable("tooltip.bayonetdurability",
+                        bayonetDurability - dmg, bayonetDurability));
+            }
+        }
     }
 
     @Override
