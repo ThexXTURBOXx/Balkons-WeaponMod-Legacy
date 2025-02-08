86ca0120679e2e5485a2d635b2f4aaefe1ff388e
Nico Mexis <nico.mexis@kabelmail.de>
2025-02-08 15:14:00 +0100

    Fix unwanted blocking of projectiles

--- src/main/java/ckathode/weaponmod/WMCommonEventHandler.java
+++ src/main/java/ckathode/weaponmod/WMCommonEventHandler.java
@@ -1,16 +1,33 @@
 package ckathode.weaponmod;
 
+import ckathode.weaponmod.item.IItemWeapon;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraftforge.event.entity.EntityEvent;
+import net.minecraftforge.event.entity.living.LivingAttackEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 
 public class WMCommonEventHandler {
     @SubscribeEvent
-    public void onEntityConstructed(EntityEvent.EntityConstructing event) {
+    public void initPlayerWeaponData(EntityEvent.EntityConstructing event) {
         Entity entity = event.getEntity();
         if (entity instanceof EntityPlayer) {
             PlayerWeaponData.initPlayerWeaponData((EntityPlayer) entity);
         }
+    }
+
+    @SubscribeEvent
+    public void cancelBlockingOfRangedWeapons(LivingAttackEvent event) {
+        EntityLivingBase entity = event.getEntityLiving();
+        if (!(entity instanceof EntityPlayer)) return;
+        EntityPlayer player = (EntityPlayer) entity;
+        ItemStack stack = player.getActiveItemStack();
+        Item item = stack.isEmpty() ? null : stack.getItem();
+        if (!(item instanceof IItemWeapon)) return;
+
+        player.resetActiveHand();
     }
 }
