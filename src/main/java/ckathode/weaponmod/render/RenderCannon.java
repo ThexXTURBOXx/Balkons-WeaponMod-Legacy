4b06085951a554c302b757039c770851e60f5946
Nico Mexis <nico.mexis@kabelmail.de>
2025-03-22 18:53:07 +0100

    Refactor legacy cannon model

--- src/main/java/ckathode/weaponmod/render/RenderCannon.java
+++ src/main/java/ckathode/weaponmod/render/RenderCannon.java
@@ -31,45 +31,37 @@ public class RenderCannon extends WMRenderer<EntityCannon> {
     public void render(EntityCannon entitycannon, float f, float f1,
                        MatrixStack ms, IRenderTypeBuffer bufs, int lm) {
         ms.push();
+        f = interpolateRotation(entitycannon.prevRotationYaw, entitycannon.rotationYaw, f1);
+
         if (BalkonsWeaponMod.instance.modConfig.legacyCannonModel.get()) {
-            modelLegacy.barrel.rotateAngleX = Math.max(-entitycannon.rotationPitch / 120.0f, -0.25f);
             ms.translate(0, 0.1, 0);
             ms.rotate(Vector3f.YP.rotationDegrees(-f));
-            final float f3 = entitycannon.getTimeSinceHit() - f1;
-            float f4 = entitycannon.getCurrentDamage() - f1;
-            if (f4 < 0.0f) {
-                f4 = 0.0f;
-            }
-            if (f3 > 0.0f) {
-                ms.rotate(Vector3f.ZP.rotationDegrees(MathHelper.sin(f3) * f3 * f4 / 10.0f * entitycannon.getRockDirection() / 5.0f));
-            }
-            IVertexBuilder builder = bufs.getBuffer(RenderType.getEntityCutout(getEntityTexture(entitycannon)));
-            ms.scale(-1.0f, -1.0f, 1.0f);
-            ms.rotate(Vector3f.XP.rotationDegrees(180.0f));
-            float f5 = 1f;
-            if (entitycannon.isSuperPowered() && entitycannon.ticksExisted % 5 < 2) f5 = 1.5f;
-            modelLegacy.render(ms, builder, lm, OverlayTexture.NO_OVERLAY,
-                    entitycannon.getBrightness() * f5, entitycannon.getBrightness() * f5,
-                    entitycannon.getBrightness() * f5, 1);
         } else {
-            float rot =
-                    entitycannon.prevRotationPitch + (entitycannon.rotationPitch - entitycannon.prevRotationPitch) * f1;
-            rot = Math.min(rot, 20.0f);
-            f = interpolateRotation(entitycannon.prevRotationYaw, entitycannon.rotationYaw, f1);
             ms.translate(0, 2.375f, 0);
             ms.rotate(Vector3f.YP.rotationDegrees(180.0f - f));
-            float f2 = entitycannon.getTimeSinceHit() - f1;
-            float f3 = entitycannon.getCurrentDamage() - f1;
-            if (f3 < 0.0f) {
-                f3 = 0.0f;
-            }
-            if (f2 > 0.0f) {
-                ms.rotate(Vector3f.ZP.rotationDegrees(MathHelper.sin(f2) * f2 * f3 / 10.0f * entitycannon.getRockDirection() / 5.0f));
-            }
-            IVertexBuilder builder = bufs.getBuffer(RenderType.getEntityCutout(getEntityTexture(entitycannon)));
+        }
+
+        float f2 = entitycannon.getTimeSinceHit() - f1;
+        float f3 = entitycannon.getCurrentDamage() - f1;
+        if (f3 < 0.0f) f3 = 0.0f;
+        if (f2 > 0.0f)
+            ms.rotate(Vector3f.ZP.rotationDegrees(MathHelper.sin(f2) * f2 * f3 / 10.0f * entitycannon.getRockDirection() / 5.0f));
+        IVertexBuilder builder = bufs.getBuffer(RenderType.getEntityCutout(getEntityTexture(entitycannon)));
+        float f4 = 1f;
+        if (entitycannon.isSuperPowered() && entitycannon.ticksExisted % 5 < 2) f4 = 1.5f;
+
+        if (BalkonsWeaponMod.instance.modConfig.legacyCannonModel.get()) {
+            ms.scale(-1.0f, -1.0f, 1.0f);
+            ms.rotate(Vector3f.XP.rotationDegrees(180.0f));
+            modelLegacy.barrel.rotateAngleX = Math.max(-entitycannon.rotationPitch / 120.0f, -0.25f);
+            modelLegacy.render(ms, builder, lm, OverlayTexture.NO_OVERLAY,
+                    entitycannon.getBrightness() * f4, entitycannon.getBrightness() * f4,
+                    entitycannon.getBrightness() * f4, 1);
+        } else {
+            float rot = entitycannon.prevRotationPitch +
+                        (entitycannon.rotationPitch - entitycannon.prevRotationPitch) * f1;
+            rot = Math.min(rot, 20.0f);
             ms.scale(-1.6f, -1.6f, 1.6f);
-            float f4 = 1f;
-            if (entitycannon.isSuperPowered() && entitycannon.ticksExisted % 5 < 2) f4 = 1.5f;
             ms.push();
             ms.translate(0.0f, 1.0f, 0.0f);
             ms.rotate(Vector3f.XP.rotationDegrees(rot));
@@ -86,6 +78,7 @@ public class RenderCannon extends WMRenderer<EntityCannon> {
                     entitycannon.getBrightness() * f4, entitycannon.getBrightness() * f4,
                     entitycannon.getBrightness() * f4, 1);
         }
+
         ms.pop();
         super.render(entitycannon, f, f1, ms, bufs, lm);
     }
