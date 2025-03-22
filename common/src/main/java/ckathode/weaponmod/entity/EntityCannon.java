adfdf7e7c801b92b0b15a0a3f456b39293f4e9b0
Nico Mexis <nico.mexis@kabelmail.de>
2025-03-22 19:30:33 +0100

    Only allow a single passenger in cannon

--- common/src/main/java/ckathode/weaponmod/entity/EntityCannon.java
+++ common/src/main/java/ckathode/weaponmod/entity/EntityCannon.java
@@ -105,6 +105,10 @@ public class EntityCannon extends Boat {
     }
 
     @Override
+    protected int getMaxPassengers() {
+        return 1;
+    }
+
     public boolean hurtServer(ServerLevel serverLevel, @NotNull DamageSource damagesource, float damage) {
         if (!isAlive()) {
             return true;
@@ -269,8 +273,8 @@ public class EntityCannon extends Boat {
         if (!isLoaded()) {
             return;
         }
-        Entity entityPassenger = getPassengers().isEmpty() ? null : getPassengers().getFirst();
-        if (!level().isClientSide) {
+        Entity entityPassenger = getControllingPassenger();
+        if (entityPassenger != null && !level().isClientSide) {
             EntityCannonBall entitycannonball = new EntityCannonBall(level(), this,
                     entityPassenger.getXRot(), entityPassenger.getYRot(), isSuperPowered(), null);
             level().addFreshEntity(entitycannonball);
