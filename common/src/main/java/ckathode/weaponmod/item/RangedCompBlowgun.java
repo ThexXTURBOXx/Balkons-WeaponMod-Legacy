826e89376f9a15808573a0c937394c216e239db1
Nico Mexis <nico.mexis@kabelmail.de>
2025-06-17 23:17:32 +0200

    Fix compatibility with InventoryBogoSorter
    Required rewrite of item consumption-related stuff
    Fixes #103

--- common/src/main/java/ckathode/weaponmod/item/RangedCompBlowgun.java
+++ common/src/main/java/ckathode/weaponmod/item/RangedCompBlowgun.java
@@ -53,10 +53,7 @@ public class RangedCompBlowgun extends RangedComponent {
         ItemStack dartStackCopy = dartstack.copy();
         if (!entityplayer.isCreative()
             && EnchantmentHelper.getItemEnchantmentLevel(Enchantments.INFINITY, itemstack) == 0) {
-            dartstack.shrink(1);
-            if (dartstack.isEmpty()) {
-                entityplayer.getInventory().removeItem(dartstack);
-            }
+            consumeAmmo(entityplayer);
         }
         if (!world.isClientSide) {
             EntityBlowgunDart entityblowgundart = new EntityBlowgunDart(world, entityplayer);
