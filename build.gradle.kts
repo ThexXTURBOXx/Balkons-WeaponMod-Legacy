6be7b3f0368154062f7b20234c66c4d4c62f35b8
Nico Mexis <nico.mexis@kabelmail.de>
2025-06-17 21:29:12 +0200

    I don't see this happening...

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ import java.time.format.DateTimeFormatter
 plugins {
     idea
     java
-    id("gg.essential.loom") version "1.6.+"
+    id("gg.essential.loom") version "1.6.9999"
     id("dev.architectury.architectury-pack200") version "0.1.3"
 }
 
