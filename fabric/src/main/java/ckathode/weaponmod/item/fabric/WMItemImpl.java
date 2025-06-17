22c0bb7ff90a2d1ca84ecb134f678b8ea0585c24
Nico Mexis <nico.mexis@kabelmail.de>
2025-06-17 23:13:35 +0200

    Fix compatibility with InventoryBogoSorter
    Required rewrite of item consumption-related stuff
    Fixes #103

---
+++ fabric/src/main/java/ckathode/weaponmod/item/fabric/WMItemImpl.java
@@ -0,0 +1,13 @@
+package ckathode.weaponmod.item.fabric;
+
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+
+public class WMItemImpl {
+
+    public static void onPlayerDestroyItem(Player player, ItemStack stack, InteractionHand hand) {
+        // There is no equivalent on Fabric - hence, do nothing
+    }
+
+}
