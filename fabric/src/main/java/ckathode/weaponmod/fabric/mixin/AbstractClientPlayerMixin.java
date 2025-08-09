d17de3ddd84887b649eb1a4774701d25c5e40f3e
Nico Mexis <nico.mexis@kabelmail.de>
2025-08-09 21:46:18 +0200

    Require MixinExtras

--- fabric/src/main/java/ckathode/weaponmod/fabric/mixin/AbstractClientPlayerMixin.java
+++ fabric/src/main/java/ckathode/weaponmod/fabric/mixin/AbstractClientPlayerMixin.java
@@ -1,20 +1,19 @@
 package ckathode.weaponmod.fabric.mixin;
 
 import ckathode.weaponmod.WMClientEventHandler;
+import com.llamalad7.mixinextras.sugar.Local;
 import net.minecraft.client.player.AbstractClientPlayer;
 import net.minecraft.world.entity.LivingEntity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
-import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 @Mixin(AbstractClientPlayer.class)
 public class AbstractClientPlayerMixin {
 
-    @Inject(method = "getFieldOfViewModifier", at = @At(value = "RETURN"), cancellable = true,
-            locals = LocalCapture.CAPTURE_FAILHARD)
-    public void getFieldOfViewModifier(CallbackInfoReturnable<Float> cir, float f) {
+    @Inject(method = "getFieldOfViewModifier", at = @At(value = "RETURN"), cancellable = true)
+    public void getFieldOfViewModifier(CallbackInfoReturnable<Float> cir, @Local float f) {
         cir.setReturnValue(WMClientEventHandler.getNewFOV((LivingEntity) (Object) this, f, cir.getReturnValueF()));
     }
 
