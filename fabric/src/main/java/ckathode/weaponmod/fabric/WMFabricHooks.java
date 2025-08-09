3965bbe619b1bd01c00a7ddc6b75838e14220af2
Nico Mexis <nico.mexis@kabelmail.de>
2025-08-09 21:36:22 +0200

    Add enchantment applicability checks

---
+++ fabric/src/main/java/ckathode/weaponmod/fabric/WMFabricHooks.java
@@ -0,0 +1,21 @@
+package ckathode.weaponmod.fabric;
+
+import ckathode.weaponmod.item.IItemWeapon;
+import ckathode.weaponmod.item.MeleeComponent;
+import ckathode.weaponmod.item.RangedComponent;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.enchantment.Enchantment;
+
+public class WMFabricHooks {
+
+    public static boolean canDefinitelyEnchant(ItemStack stack, Enchantment enchantment) {
+        if (stack.getItem() instanceof IItemWeapon weapon) {
+            MeleeComponent meleeComponent = weapon.getMeleeComponent();
+            RangedComponent rangedComponent = weapon.getRangedComponent();
+            return (meleeComponent != null && meleeComponent.canApplyEnchantment(stack, enchantment)) ||
+                   (rangedComponent != null && rangedComponent.canApplyEnchantment(stack, enchantment));
+        }
+        return false;
+    }
+
+}
---
+++ fabric/src/main/java/ckathode/weaponmod/fabric/mixin/EnchantmentHelperMixin.java
@@ -0,0 +1,48 @@
+package ckathode.weaponmod.fabric.mixin;
+
+import ckathode.weaponmod.fabric.WMFabricHooks;
+import java.util.Iterator;
+import java.util.List;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.enchantment.Enchantment;
+import net.minecraft.world.item.enchantment.EnchantmentCategory;
+import net.minecraft.world.item.enchantment.EnchantmentHelper;
+import net.minecraft.world.item.enchantment.EnchantmentInstance;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
+
+@Mixin(EnchantmentHelper.class)
+public abstract class EnchantmentHelperMixin {
+
+    @Unique
+    private static ItemStack stack;
+
+    @Unique
+    private static Enchantment enchantment;
+
+    @Inject(method = "getAvailableEnchantmentResults",
+            at = @At(value = "INVOKE", target = "Lnet/minecraft/world/item/enchantment/EnchantmentCategory;canEnchant" +
+                                                "(Lnet/minecraft/world/item/Item;)Z"),
+            locals = LocalCapture.CAPTURE_FAILHARD)
+    private static void fetchLocals(int level, ItemStack stack, boolean allowTreasure,
+                                    CallbackInfoReturnable<List<EnchantmentInstance>> cir,
+                                    List<EnchantmentInstance> list, Item item, boolean bl, Iterator<Enchantment> iter,
+                                    Enchantment enchantment) {
+        EnchantmentHelperMixin.stack = stack;
+        EnchantmentHelperMixin.enchantment = enchantment;
+    }
+
+    @Redirect(method = "getAvailableEnchantmentResults",
+            at = @At(value = "INVOKE", target = "Lnet/minecraft/world/item/enchantment/EnchantmentCategory;canEnchant" +
+                                                "(Lnet/minecraft/world/item/Item;)Z"))
+    private static boolean canEnchant(EnchantmentCategory instance, Item item) {
+        return WMFabricHooks.canDefinitelyEnchant(stack, enchantment) || instance.canEnchant(item);
+    }
+
+}
