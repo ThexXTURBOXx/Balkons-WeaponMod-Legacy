7b6cd499ead81bdfac811c664896172e5a581169
Nico Mexis <nico.mexis@kabelmail.de>
2025-08-16 22:22:35 +0200

    Update Loom

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ import java.time.format.DateTimeFormatter
 plugins {
     idea
     java
-    id("gg.essential.loom") version "1.9.+"
+    id("gg.essential.loom") version "1.10.+"
     id("dev.architectury.architectury-pack200") version "0.1.3"
 }
 
