19d0f40b866b3d30bd1d5bef3a39479a0dcc318e
Nico Mexis <nico.mexis@kabelmail.de>
2025-08-16 20:21:59 +0200

    Make fire rod enchantments functional
    Fixes #112

--- common/src/main/java/ckathode/weaponmod/item/MeleeCompFirerod.java
+++ common/src/main/java/ckathode/weaponmod/item/MeleeCompFirerod.java
@@ -2,7 +2,9 @@ package ckathode.weaponmod.item;
 
 import ckathode.weaponmod.BalkonsWeaponMod;
 import ckathode.weaponmod.WMItemBuilder;
+import net.minecraft.core.Holder;
 import net.minecraft.core.particles.ParticleTypes;
+import net.minecraft.core.registries.Registries;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.util.Mth;
 import net.minecraft.world.entity.Entity;
@@ -13,6 +15,9 @@ import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.ItemUseAnimation;
 import net.minecraft.world.item.ToolMaterial;
+import net.minecraft.world.item.enchantment.Enchantment;
+import net.minecraft.world.item.enchantment.EnchantmentHelper;
+import net.minecraft.world.item.enchantment.Enchantments;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -29,7 +34,11 @@ public class MeleeCompFirerod extends MeleeComponent {
     public void hurtEnemy(@NotNull ItemStack itemstack, @NotNull LivingEntity entityliving,
                           @NotNull LivingEntity entityliving1) {
         super.hurtEnemy(itemstack, entityliving, entityliving1);
-        entityliving.igniteForSeconds(12 + entityliving.getRandom().nextInt(3));
+        Holder<Enchantment> fireAspect = entityliving.registryAccess()
+                .lookupOrThrow(Registries.ENCHANTMENT).get(Enchantments.FIRE_ASPECT).orElse(null);
+        int enchBonus = fireAspect != null
+                ? 2 * EnchantmentHelper.getItemEnchantmentLevel(fireAspect, itemstack) : 0;
+        entityliving.igniteForSeconds(12 + enchBonus + entityliving.getRandom().nextInt(3));
     }
 
     @Override
