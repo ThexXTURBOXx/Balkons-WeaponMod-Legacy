720a3b6bd00cd93e22d5e52c6f48d021bcb4fdc6
Nico Mexis <nico.mexis@kabelmail.de>
2025-08-16 19:14:21 +0200

    Make fire rod enchantments functional
    Fixes #112

--- common/src/main/java/ckathode/weaponmod/item/MeleeCompFirerod.java
+++ common/src/main/java/ckathode/weaponmod/item/MeleeCompFirerod.java
@@ -2,7 +2,9 @@ package ckathode.weaponmod.item;
 
 import ckathode.weaponmod.BalkonsWeaponMod;
 import ckathode.weaponmod.WMItemBuilder;
+import net.minecraft.core.Holder;
 import net.minecraft.core.particles.ParticleTypes;
+import net.minecraft.core.registries.Registries;
 import net.minecraft.util.Mth;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.HumanoidArm;
@@ -11,6 +13,9 @@ import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.ItemUseAnimation;
 import net.minecraft.world.item.ToolMaterial;
+import net.minecraft.world.item.enchantment.Enchantment;
+import net.minecraft.world.item.enchantment.EnchantmentHelper;
+import net.minecraft.world.item.enchantment.Enchantments;
 import net.minecraft.world.level.Level;
 import org.jetbrains.annotations.NotNull;
 
@@ -24,10 +29,15 @@ public class MeleeCompFirerod extends MeleeComponent {
     }
 
     @Override
-    public boolean hurtEnemy(ItemStack itemstack, LivingEntity entityliving, LivingEntity entityliving1) {
-        boolean flag = super.hurtEnemy(itemstack, entityliving, entityliving1);
+    public boolean hurtEnemy(ItemStack itemstack, LivingEntity entityliving, LivingEntity attacker) {
+        boolean flag = super.hurtEnemy(itemstack, entityliving, attacker);
         if (flag) {
-            entityliving.igniteForSeconds(12 + entityliving.getRandom().nextInt(3));
+            Holder<Enchantment> fireAspect = entityliving.registryAccess()
+                    .lookupOrThrow(Registries.ENCHANTMENT).get(Enchantments.FIRE_ASPECT).orElse(null);
+            int enchBonus = fireAspect != null
+                    ? 2 * EnchantmentHelper.getItemEnchantmentLevel(fireAspect, itemstack) : 0;
+            entityliving.igniteForSeconds(12 + enchBonus +
+                                          entityliving.getRandom().nextInt(3));
         }
         return flag;
     }
