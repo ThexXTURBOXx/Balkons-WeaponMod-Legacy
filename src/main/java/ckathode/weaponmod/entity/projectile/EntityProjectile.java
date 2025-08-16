17e599be1b1df2ebac88163539049e856d6f84fc
Nico Mexis <nico.mexis@kabelmail.de>
2025-08-16 14:19:10 +0200

    Remove unnecessary assertion

--- src/main/java/ckathode/weaponmod/entity/projectile/EntityProjectile.java
+++ src/main/java/ckathode/weaponmod/entity/projectile/EntityProjectile.java
@@ -346,7 +346,7 @@ public class EntityProjectile extends EntityArrow implements IThrowableEntity, I
                 WEAPON_TARGETS);
         double d = 0.0;
         for (Entity entity2 : list) {
-            if (entity2.canBeCollidedWith() && (entity2 != shootingEntity || ticksInAir >= 5)) {
+            if (entity2 != shootingEntity || ticksInAir >= 5) {
                 double exp = 0.3;
                 AxisAlignedBB axisalignedbb = entity2.getEntityBoundingBox().expand(exp, exp, exp);
                 MovingObjectPosition raytraceresult = axisalignedbb.calculateIntercept(vec3d, vec3d1);
