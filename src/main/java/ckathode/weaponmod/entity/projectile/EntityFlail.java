93f1e260f751f776c6c1e7c9c873bf59c8d61d8c
Nico Mexis <nico.mexis@kabelmail.de>
2025-08-16 17:28:06 +0200

    Make flails more stable and add basic enchantment support
    Fixes #113

--- src/main/java/ckathode/weaponmod/entity/projectile/EntityFlail.java
+++ src/main/java/ckathode/weaponmod/entity/projectile/EntityFlail.java
@@ -75,7 +75,9 @@ public class EntityFlail extends EntityMaterialProjectile<EntityFlail> {
             if (shooter instanceof EntityLivingBase) {
                 ItemStack itemstack = ((EntityLivingBase) shooter).getHeldItemMainhand();
                 ItemStack thrownItem = getWeapon();
-                if (itemstack.isEmpty() || (!thrownItem.isEmpty() && itemstack.getItem() != thrownItem.getItem()) || !shooter.isAlive()) {
+                if (itemstack.isEmpty() ||
+                    (!thrownItem.isEmpty() && !ItemStack.areItemStacksEqual(itemstack, thrownItem)) ||
+                    !shooter.isAlive()) {
                     pickUpByOwner();
                 }
             }
@@ -159,6 +161,7 @@ public class EntityFlail extends EntityMaterialProjectile<EntityFlail> {
                 ? DamageSource.causeMobDamage((EntityLivingBase) shooter)
                 : WeaponDamageSource.causeProjectileWeaponDamage(this, shooter);
         if (entity.attackEntityFrom(damagesource, flailDamage + extraDamage)) {
+            applyEntityHitEffects(entity);
             playHitSound();
             returnToOwner(true);
         } else {
