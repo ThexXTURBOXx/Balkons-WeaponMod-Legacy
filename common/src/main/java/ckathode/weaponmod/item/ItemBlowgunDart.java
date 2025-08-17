a5ce6c63b12eeaf1e0d874243d644b7f18945c0f
Nico Mexis <nico.mexis@kabelmail.de>
2025-08-17 21:01:28 +0200

    Rewrite major parts of blowgun darts

--- common/src/main/java/ckathode/weaponmod/item/ItemBlowgunDart.java
+++ common/src/main/java/ckathode/weaponmod/item/ItemBlowgunDart.java
@@ -2,13 +2,13 @@ package ckathode.weaponmod.item;
 
 import ckathode.weaponmod.WMItemBuilder;
 import com.mojang.datafixers.util.Pair;
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
-import java.util.Objects;
 import java.util.stream.Collectors;
 import net.minecraft.network.chat.Component;
+import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.item.alchemy.PotionUtils;
@@ -19,10 +19,9 @@ import org.jetbrains.annotations.Nullable;
 public class ItemBlowgunDart extends WMItem {
 
     public static final String ID_PREFIX = "dart";
-    public static final Map<DartType, ItemBlowgunDart> ITEMS =
-            Arrays.stream(DartType.dartTypes).filter(Objects::nonNull)
-                    .map(t -> new Pair<>(t, WMItemBuilder.createStandardBlowgunDart(t)))
-                    .collect(Collectors.toMap(Pair::getFirst, Pair::getSecond));
+    public static final Map<DartType, ItemBlowgunDart> ITEMS = DartType.DART_TYPES.stream()
+            .map(t -> new Pair<>(t, WMItemBuilder.createStandardBlowgunDart(t)))
+            .collect(Collectors.toMap(Pair::getFirst, Pair::getSecond));
 
     @NotNull
     private final DartType dartType;
@@ -35,12 +34,30 @@ public class ItemBlowgunDart extends WMItem {
     @Override
     public void appendHoverText(@NotNull ItemStack itemstack, @Nullable Level worldIn,
                                 @NotNull List<Component> list, @NotNull TooltipFlag flag) {
-        PotionUtils.addPotionTooltip(Collections.singletonList(dartType.potionEffect()), list, 1.0f);
+        PotionUtils.addPotionTooltip(dartType.potionEffects(), list, 1.0f);
     }
 
     @NotNull
     public DartType getDartType() {
         return dartType;
+    }
+
+    @NotNull
+    private static DartType getDartType(@NotNull ItemStack stack) {
+        return stack.getItem() instanceof ItemBlowgunDart dart ? dart.getDartType() : DartType.DAMAGE;
+    }
+
+    public static List<MobEffectInstance> getEffects(@NotNull ItemStack stack) {
+        if (stack.isEmpty()) return Collections.emptyList();
+
+        List<MobEffectInstance> effects = new ArrayList<>(getDartType(stack).potionEffects());
+        PotionUtils.getCustomEffects(stack.getTag(), effects);
+        return effects;
+    }
+
+    public static float[] getColor(ItemStack stack) {
+        if (stack.isEmpty()) return DartType.DAMAGE.color();
+        return getDartType(stack).color();
     }
 
 }
