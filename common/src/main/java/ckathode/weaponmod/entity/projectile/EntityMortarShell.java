618be9e43a90f4a59ee99e136bed406a2f07563a
Nico Mexis <nico.mexis@kabelmail.de>
2025-08-17 11:12:24 +0200

    Fix a few other enchantments not being applied correctly

--- common/src/main/java/ckathode/weaponmod/entity/projectile/EntityMortarShell.java
+++ common/src/main/java/ckathode/weaponmod/entity/projectile/EntityMortarShell.java
@@ -6,10 +6,7 @@ import ckathode.weaponmod.WMRegistries;
 import ckathode.weaponmod.WeaponModConfig;
 import dev.architectury.networking.NetworkManager;
 import net.minecraft.core.BlockPos;
-import net.minecraft.core.Holder;
-import net.minecraft.core.Registry;
 import net.minecraft.core.particles.ParticleTypes;
-import net.minecraft.core.registries.Registries;
 import net.minecraft.network.protocol.Packet;
 import net.minecraft.network.protocol.game.ClientGamePacketListener;
 import net.minecraft.server.level.ServerEntity;
@@ -21,9 +18,6 @@ import net.minecraft.world.entity.EntityDimensions;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.enchantment.Enchantment;
-import net.minecraft.world.item.enchantment.EnchantmentHelper;
-import net.minecraft.world.item.enchantment.Enchantments;
 import net.minecraft.world.level.Explosion;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.phys.BlockHitResult;
@@ -96,18 +90,11 @@ public class EntityMortarShell extends EntityProjectile<EntityMortarShell> {
             return;
         }
         remove(RemovalReason.DISCARDED);
-        Entity shooter = getOwner();
-        if (!(shooter instanceof LivingEntity livingEntity)) return;
-        Registry<Enchantment> enchRegistry = registryAccess().registryOrThrow(Registries.ENCHANTMENT);
-        Holder<Enchantment> power = enchRegistry.getHolder(Enchantments.POWER).orElse(null);
-        Holder<Enchantment> flame = enchRegistry.getHolder(Enchantments.FLAME).orElse(null);
-        if (power != null && EnchantmentHelper.getEnchantmentLevel(power, livingEntity) > 0) {
-            float f1 = EnchantmentHelper.getEnchantmentLevel(power, livingEntity);
-            explosiveSize += f1 / 4.0f;
+        if (extraDamage > 0) {
+            explosiveSize += extraDamage / 4.0f;
         }
-        boolean flag = flame != null && EnchantmentHelper.getEnchantmentLevel(flame, livingEntity) > 0;
         PhysHelper.createAdvancedExplosion(level(), this, getX(), getY(), getZ(), explosiveSize,
-                WeaponModConfig.get().mortarDoesBlockDamage, true, flag,
+                WeaponModConfig.get().mortarDoesBlockDamage, true, isOnFire(),
                 Explosion.BlockInteraction.DESTROY);
     }
 
