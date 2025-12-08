5affefb0091923f7f8c3e6af82662c89d8575a63
Nico Mexis <nico.mexis@kabelmail.de>
2025-12-08 23:20:23 +0100

    Remove all @Environment and @OnlyIn occurrences
    Apparently, that annotation is discouraged...
    Also fixes #119

--- forge/src/main/java/ckathode/weaponmod/forge/WMClientEventHandlerForge.java
+++ forge/src/main/java/ckathode/weaponmod/forge/WMClientEventHandlerForge.java
@@ -2,14 +2,11 @@ package ckathode.weaponmod.forge;
 
 import ckathode.weaponmod.WMClientEventHandler;
 import ckathode.weaponmod.render.GuiOverlayReloaded;
-import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.client.event.FOVModifierEvent;
 import net.minecraftforge.client.event.RenderGameOverlayEvent;
 import net.minecraftforge.client.gui.ForgeIngameGui;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 
-@OnlyIn(Dist.CLIENT)
 public class WMClientEventHandlerForge {
 
     @SubscribeEvent
