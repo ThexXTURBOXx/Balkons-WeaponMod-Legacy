806d13ef2ea248d71d0de1df4b36949cbde8c229
Nico Mexis <nico.mexis@kabelmail.de>
2025-12-08 23:18:34 +0100

    Remove all @Environment and @OnlyIn occurrences
    Apparently, that annotation is discouraged...
    Also fixes #119

--- common/src/main/java/ckathode/weaponmod/network/WMMessage.java
+++ common/src/main/java/ckathode/weaponmod/network/WMMessage.java
@@ -3,15 +3,12 @@ package ckathode.weaponmod.network;
 import dev.architectury.networking.NetworkManager;
 import io.netty.buffer.ByteBuf;
 import java.util.function.Supplier;
-import net.fabricmc.api.EnvType;
-import net.fabricmc.api.Environment;
 
 public interface WMMessage<T extends WMMessage<T>> {
     void encode(ByteBuf buf);
 
     void decode(ByteBuf buf);
 
-    @Environment(EnvType.CLIENT)
     void handleClientSide(T msg, Supplier<NetworkManager.PacketContext> ctx);
 
     void handleServerSide(T msg, Supplier<NetworkManager.PacketContext> ctx);
