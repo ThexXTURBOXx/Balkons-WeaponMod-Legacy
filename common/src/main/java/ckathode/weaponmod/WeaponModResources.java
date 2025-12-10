ad46ca6b2646ec2b4e2814e9b3a920f25477ca98
Nico Mexis <nico.mexis@kabelmail.de>
2025-12-10 22:15:34 +0100

    Migrate remaining ResourceLocation usages to BWM#id method

--- common/src/main/java/ckathode/weaponmod/WeaponModResources.java
+++ common/src/main/java/ckathode/weaponmod/WeaponModResources.java
@@ -2,40 +2,24 @@ package ckathode.weaponmod;
 
 import net.minecraft.resources.ResourceLocation;
 
-import static ckathode.weaponmod.BalkonsWeaponMod.MOD_ID;
-
 public final class WeaponModResources {
     public static final class Entity {
-        public static final ResourceLocation DART = ResourceLocation.fromNamespaceAndPath(
-                MOD_ID, "textures/entity/blowgun_dart.png");
-        public static final ResourceLocation BOOMERANG = ResourceLocation.fromNamespaceAndPath(
-                MOD_ID, "textures/entity/boomerang.png");
-        public static final ResourceLocation CANNON = ResourceLocation.fromNamespaceAndPath(
-                MOD_ID, "textures/entity/cannon.png");
-        public static final ResourceLocation CANNON_LEGACY = ResourceLocation.fromNamespaceAndPath(
-                MOD_ID, "textures/entity/cannon_legacy.png");
-        public static final ResourceLocation CANNONBALL = ResourceLocation.fromNamespaceAndPath(
-                MOD_ID, "textures/entity/cannon_ball.png");
-        public static final ResourceLocation BOLT = ResourceLocation.fromNamespaceAndPath(
-                MOD_ID, "textures/entity/crossbow_bolt.png");
-        public static final ResourceLocation DUMMY = ResourceLocation.fromNamespaceAndPath(
-                MOD_ID, "textures/entity/dummy.png");
-        public static final ResourceLocation DYNAMITE = ResourceLocation.fromNamespaceAndPath(
-                MOD_ID, "textures/entity/dynamite.png");
-        public static final ResourceLocation FLAIL = ResourceLocation.fromNamespaceAndPath(
-                MOD_ID, "textures/entity/flail.png");
-        public static final ResourceLocation JAVELIN = ResourceLocation.fromNamespaceAndPath(
-                MOD_ID, "textures/entity/spear.png");
-        public static final ResourceLocation KNIFE = ResourceLocation.fromNamespaceAndPath(
-                MOD_ID, "textures/entity/knife.png");
-        public static final ResourceLocation BULLET = ResourceLocation.fromNamespaceAndPath(
-                MOD_ID, "textures/entity/musket_bullet.png");
-        public static final ResourceLocation SPEAR = ResourceLocation.fromNamespaceAndPath(
-                MOD_ID, "textures/entity/spear.png");
+        public static final ResourceLocation DART = BalkonsWeaponMod.id("textures/entity/blowgun_dart.png");
+        public static final ResourceLocation BOOMERANG = BalkonsWeaponMod.id("textures/entity/boomerang.png");
+        public static final ResourceLocation CANNON = BalkonsWeaponMod.id("textures/entity/cannon.png");
+        public static final ResourceLocation CANNON_LEGACY = BalkonsWeaponMod.id("textures/entity/cannon_legacy.png");
+        public static final ResourceLocation CANNONBALL = BalkonsWeaponMod.id("textures/entity/cannon_ball.png");
+        public static final ResourceLocation BOLT = BalkonsWeaponMod.id("textures/entity/crossbow_bolt.png");
+        public static final ResourceLocation DUMMY = BalkonsWeaponMod.id("textures/entity/dummy.png");
+        public static final ResourceLocation DYNAMITE = BalkonsWeaponMod.id("textures/entity/dynamite.png");
+        public static final ResourceLocation FLAIL = BalkonsWeaponMod.id("textures/entity/flail.png");
+        public static final ResourceLocation JAVELIN = BalkonsWeaponMod.id("textures/entity/spear.png");
+        public static final ResourceLocation KNIFE = BalkonsWeaponMod.id("textures/entity/knife.png");
+        public static final ResourceLocation BULLET = BalkonsWeaponMod.id("textures/entity/musket_bullet.png");
+        public static final ResourceLocation SPEAR = BalkonsWeaponMod.id("textures/entity/spear.png");
     }
 
     public static final class Gui {
-        public static final ResourceLocation OVERLAY = ResourceLocation.fromNamespaceAndPath(
-                MOD_ID, "textures/gui/overlay.png");
+        public static final ResourceLocation OVERLAY = BalkonsWeaponMod.id("textures/gui/overlay.png");
     }
 }
