9da5902180f7c5f252a16f799240f31eaa8da8e6
Nico Mexis <nico.mexis@kabelmail.de>
2025-12-10 22:18:20 +0100

    Migrate remaining ResourceLocation usages to BWM#id method

--- common/src/main/java/ckathode/weaponmod/network/MsgExplosion.java
+++ common/src/main/java/ckathode/weaponmod/network/MsgExplosion.java
@@ -1,6 +1,7 @@
 package ckathode.weaponmod.network;
 
 import ckathode.weaponmod.AdvancedExplosion;
+import ckathode.weaponmod.BalkonsWeaponMod;
 import dev.architectury.networking.NetworkManager;
 import java.util.List;
 import net.minecraft.core.BlockPos;
@@ -8,18 +9,14 @@ import net.minecraft.network.RegistryFriendlyByteBuf;
 import net.minecraft.network.codec.ByteBufCodecs;
 import net.minecraft.network.codec.StreamCodec;
 import net.minecraft.network.protocol.common.custom.CustomPacketPayload;
-import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.phys.Vec3;
 import org.jetbrains.annotations.NotNull;
 
-import static ckathode.weaponmod.BalkonsWeaponMod.MOD_ID;
-
 public record MsgExplosion(Vec3 center, float size, List<BlockPos> blocks, boolean smallParticles,
                            boolean bigParticles) implements CustomPacketPayload {
 
-    public static final Type<MsgExplosion> EXPLOSION_PACKET_TYPE =
-            new Type<>(ResourceLocation.fromNamespaceAndPath(MOD_ID, "explosion"));
+    public static final Type<MsgExplosion> EXPLOSION_PACKET_TYPE = new Type<>(BalkonsWeaponMod.id("explosion"));
     public static final StreamCodec<RegistryFriendlyByteBuf, MsgExplosion> STREAM_CODEC = StreamCodec.composite(
             Vec3.STREAM_CODEC, MsgExplosion::center,
             ByteBufCodecs.FLOAT, MsgExplosion::size,
