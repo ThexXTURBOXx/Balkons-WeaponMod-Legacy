1dc2ab4752ddde801dbe53b586c5d165484720e1
Nico Mexis <nico.mexis@kabelmail.de>
2025-12-10 21:36:12 +0100

    Backport BWM#id method

--- common/src/main/java/ckathode/weaponmod/WMDamageSources.java
+++ common/src/main/java/ckathode/weaponmod/WMDamageSources.java
@@ -2,16 +2,13 @@ package ckathode.weaponmod;
 
 import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.ResourceKey;
-import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.damagesource.DamageType;
-
-import static ckathode.weaponmod.BalkonsWeaponMod.MOD_ID;
 
 public final class WMDamageSources {
 
     public static final ResourceKey<DamageType> BATTLEAXE = ResourceKey.create(
-            Registries.DAMAGE_TYPE, new ResourceLocation(MOD_ID, "battleaxe"));
+            Registries.DAMAGE_TYPE, BalkonsWeaponMod.id("battleaxe"));
     public static final ResourceKey<DamageType> WEAPON = ResourceKey.create(
-            Registries.DAMAGE_TYPE, new ResourceLocation(MOD_ID, "weapon"));
+            Registries.DAMAGE_TYPE, BalkonsWeaponMod.id("weapon"));
 
 }
