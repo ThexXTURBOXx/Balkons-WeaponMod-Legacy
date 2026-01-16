c088e2cc016a310fbaca33c4cd95a15936f90aa7
Nico Mexis <nico.mexis@kabelmail.de>
2026-01-16 22:13:51 +0100

    Update to Loom 1.13 and Gradle 9.3.0
    Also enable configuration cache to make subsequent compiles faster

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ import java.time.format.DateTimeFormatter
 plugins {
     idea
     java
-    id("gg.essential.loom") version "1.10.+"
+    id("gg.essential.loom") version "1.13.+"
     id("dev.architectury.architectury-pack200") version "0.1.3"
 }
 
@@ -60,6 +60,10 @@ repositories {
     mavenCentral()
     maven("https://repo.spongepowered.org/maven/")
     maven("https://maven.minecraftforge.net/")
+}
+
+tasks.test {
+    enabled = false
 }
 
 dependencies {
