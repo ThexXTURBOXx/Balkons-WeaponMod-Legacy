97d51966a0afc8855ac27a313a13828c180fc8a2
Nico Mexis <nico.mexis@kabelmail.de>
2026-06-03 19:34:52 +0200

    Fix weapon damage logic in creative
    Fixes #122

--- common/src/main/java/ckathode/weaponmod/WMCommonEventHandler.java
+++ common/src/main/java/ckathode/weaponmod/WMCommonEventHandler.java
@@ -3,6 +3,7 @@ package ckathode.weaponmod;
 import ckathode.weaponmod.item.IItemWeapon;
 import dev.architectury.event.EventResult;
 import dev.architectury.event.events.common.EntityEvent;
+import net.minecraft.tags.DamageTypeTags;
 import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.LivingEntity;
@@ -22,6 +23,8 @@ public class WMCommonEventHandler {
         ItemStack stack = entity.getUseItem();
         Item item = stack.isEmpty() ? null : stack.getItem();
         if (!(item instanceof IItemWeapon)) return EventResult.pass();
+        if (entity instanceof Player && ((Player) entity).isCreative() &&
+            !source.is(DamageTypeTags.BYPASSES_INVULNERABILITY)) return EventResult.pass();
 
         entity.stopUsingItem();
         return EventResult.pass();
