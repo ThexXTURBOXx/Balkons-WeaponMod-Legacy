0802c072339e0161256b96f55170c9588d3191b0
Nico Mexis <nico.mexis@kabelmail.de>
2026-07-16 22:51:08 +0200

    Instakill projectiles when weapon is disabled in config

--- common/src/main/java/ckathode/weaponmod/entity/projectile/EntityBlowgunDart.java
+++ common/src/main/java/ckathode/weaponmod/entity/projectile/EntityBlowgunDart.java
@@ -3,11 +3,8 @@ package ckathode.weaponmod.entity.projectile;
 import ckathode.weaponmod.WMDamageSources;
 import ckathode.weaponmod.WMRegistries;
 import ckathode.weaponmod.WMUtil;
+import ckathode.weaponmod.WeaponModConfig;
 import ckathode.weaponmod.item.ItemBlowgunDart;
-import dev.architectury.networking.NetworkManager;
-import net.minecraft.network.protocol.Packet;
-import net.minecraft.network.protocol.game.ClientGamePacketListener;
-import net.minecraft.server.level.ServerEntity;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.util.Mth;
@@ -48,10 +45,9 @@ public class EntityBlowgunDart extends EntityMaterialProjectile<EntityBlowgunDar
         setThrownItemStack(dart);
     }
 
-    @NotNull
     @Override
-    public Packet<ClientGamePacketListener> getAddEntityPacket(ServerEntity serverEntity) {
-        return NetworkManager.createAddEntityPacket(this, serverEntity);
+    protected boolean isDisabled() {
+        return !WeaponModConfig.get().isEnabled("blowgun");
     }
 
     @Override
