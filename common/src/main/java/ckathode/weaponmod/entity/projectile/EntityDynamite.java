5ce7b68a2289069e75a758fb0c76d0444e3e81ba
Nico Mexis <nico.mexis@kabelmail.de>
2026-07-16 20:46:51 +0200

    Instakill projectiles when weapon is disabled in config

--- common/src/main/java/ckathode/weaponmod/entity/projectile/EntityDynamite.java
+++ common/src/main/java/ckathode/weaponmod/entity/projectile/EntityDynamite.java
@@ -4,13 +4,9 @@ import ckathode.weaponmod.PhysHelper;
 import ckathode.weaponmod.WMDamageSources;
 import ckathode.weaponmod.WMRegistries;
 import ckathode.weaponmod.WeaponModConfig;
-import dev.architectury.networking.NetworkManager;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.core.particles.ParticleTypes;
-import net.minecraft.network.protocol.Packet;
-import net.minecraft.network.protocol.game.ClientGamePacketListener;
-import net.minecraft.server.level.ServerEntity;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.util.Mth;
@@ -61,10 +57,9 @@ public class EntityDynamite extends EntityProjectile<EntityDynamite> {
         explodefuse = i;
     }
 
-    @NotNull
     @Override
-    public Packet<ClientGamePacketListener> getAddEntityPacket(ServerEntity serverEntity) {
-        return NetworkManager.createAddEntityPacket(this, serverEntity);
+    protected boolean isDisabled() {
+        return !WeaponModConfig.get().isEnabled("dynamite");
     }
 
     @Override
