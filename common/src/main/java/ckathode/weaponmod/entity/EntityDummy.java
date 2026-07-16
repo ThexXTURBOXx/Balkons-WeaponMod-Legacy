c70c452b8f227d4b46d6ab0558154e9a654142fa
Nico Mexis <nico.mexis@kabelmail.de>
2026-07-16 22:48:32 +0200

    Instakill projectiles when weapon is disabled in config

--- common/src/main/java/ckathode/weaponmod/entity/EntityDummy.java
+++ common/src/main/java/ckathode/weaponmod/entity/EntityDummy.java
@@ -3,6 +3,7 @@ package ckathode.weaponmod.entity;
 import ckathode.weaponmod.WMDamageSources;
 import ckathode.weaponmod.WMRegistries;
 import ckathode.weaponmod.WMUtil;
+import ckathode.weaponmod.WeaponModConfig;
 import ckathode.weaponmod.item.IItemWeapon;
 import dev.architectury.networking.NetworkManager;
 import java.util.List;
@@ -154,6 +155,10 @@ public class EntityDummy extends Entity {
 
     @Override
     public void tick() {
+        if (!WeaponModConfig.get().isEnabled("dummy")) {
+            remove(RemovalReason.DISCARDED);
+            return;
+        }
         super.tick();
         int i = getTimeSinceHit();
         if (i > 0) {
