134e78f36fd2bf32ff11a23a2ada98151823d246
Nico Mexis <nico.mexis@kabelmail.de>
2026-07-16 20:46:51 +0200

    Instakill projectiles when weapon is disabled in config

--- common/src/main/java/ckathode/weaponmod/entity/projectile/EntityBlowgunDart.java
+++ common/src/main/java/ckathode/weaponmod/entity/projectile/EntityBlowgunDart.java
@@ -2,11 +2,8 @@ package ckathode.weaponmod.entity.projectile;
 
 import ckathode.weaponmod.WMDamageSources;
 import ckathode.weaponmod.WMRegistries;
+import ckathode.weaponmod.WeaponModConfig;
 import ckathode.weaponmod.item.ItemBlowgunDart;
-import dev.architectury.networking.NetworkManager;
-import net.minecraft.network.protocol.Packet;
-import net.minecraft.network.protocol.game.ClientGamePacketListener;
-import net.minecraft.server.level.ServerEntity;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.util.Mth;
@@ -47,10 +44,9 @@ public class EntityBlowgunDart extends EntityMaterialProjectile<EntityBlowgunDar
         setThrownItemStack(dart);
     }
 
-    @NotNull
     @Override
-    public Packet<ClientGamePacketListener> getAddEntityPacket(ServerEntity serverEntity) {
-        return NetworkManager.createAddEntityPacket(this, serverEntity);
+    protected boolean isDisabled() {
+        return !WeaponModConfig.get().isEnabled("blowgun");
     }
 
     @Override
--- common/src/main/java/ckathode/weaponmod/entity/projectile/EntitySpear.java
+++ common/src/main/java/ckathode/weaponmod/entity/projectile/EntitySpear.java
@@ -2,11 +2,8 @@ package ckathode.weaponmod.entity.projectile;
 
 import ckathode.weaponmod.WMDamageSources;
 import ckathode.weaponmod.WMRegistries;
+import ckathode.weaponmod.WeaponModConfig;
 import ckathode.weaponmod.item.IItemWeapon;
-import dev.architectury.networking.NetworkManager;
-import net.minecraft.network.protocol.Packet;
-import net.minecraft.network.protocol.game.ClientGamePacketListener;
-import net.minecraft.server.level.ServerEntity;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.sounds.SoundEvents;
@@ -46,10 +43,9 @@ public class EntitySpear extends EntityMaterialProjectile<EntitySpear> {
         setThrownItemStack(itemstack);
     }
 
-    @NotNull
     @Override
-    public Packet<ClientGamePacketListener> getAddEntityPacket(ServerEntity serverEntity) {
-        return NetworkManager.createAddEntityPacket(this, serverEntity);
+    protected boolean isDisabled() {
+        return !WeaponModConfig.get().isEnabled("spear");
     }
 
     @Override
