18c029353d3dce003101f8cfdd16657bdcbed98e
Nico Mexis <nico.mexis@kabelmail.de>
2026-07-17 17:13:32 +0200

    Add weapons to loot tables
    Only Forge is functional at the moment. Fabric needs a lot more work, apparently

--- common/src/main/java/ckathode/weaponmod/WMCommonEventHandler.java
+++ common/src/main/java/ckathode/weaponmod/WMCommonEventHandler.java
@@ -1,8 +1,12 @@
 package ckathode.weaponmod;
 
 import ckathode.weaponmod.item.IItemWeapon;
+import dev.architectury.event.Event;
+import dev.architectury.event.EventFactory;
 import dev.architectury.event.EventResult;
 import dev.architectury.event.events.common.EntityEvent;
+import java.util.function.Function;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.Entity;
@@ -10,8 +14,58 @@ import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.storage.loot.BuiltInLootTables;
+import net.minecraft.world.level.storage.loot.LootPool;
+import net.minecraft.world.level.storage.loot.LootTables;
+import net.minecraft.world.level.storage.loot.entries.LootItem;
+import net.minecraft.world.level.storage.loot.functions.EnchantRandomlyFunction;
+import net.minecraft.world.level.storage.loot.functions.EnchantWithLevelsFunction;
+import net.minecraft.world.level.storage.loot.functions.SetItemCountFunction;
+import net.minecraft.world.level.storage.loot.functions.SetItemDamageFunction;
+import net.minecraft.world.level.storage.loot.providers.number.ConstantValue;
+import net.minecraft.world.level.storage.loot.providers.number.UniformGenerator;
+import org.jetbrains.annotations.ApiStatus;
 
 public class WMCommonEventHandler {
+
+    public static final Event<ModifyLootTable> MODIFY_LOOT_TABLE = EventFactory.createLoop();
+
+    @FunctionalInterface
+    public interface ModifyLootTable {
+        /**
+         * Modifies a loot table.
+         *
+         * @param registries the registries provider
+         * @param id         the loot table id
+         * @param context    the context used to modify the loot table
+         * @param builtin    if {@code true}, the loot table is built-in;
+         *                   if {@code false}, it is from a user data pack
+         */
+        void modifyLootTable(LootTables registries, ResourceLocation id,
+                             LootTableModificationContext context, boolean builtin);
+    }
+
+    /**
+     * A platform-specific bridge for modifying a specific loot table.
+     */
+    @ApiStatus.NonExtendable
+    public interface LootTableModificationContext {
+        /**
+         * Adds a pool to the loot table.
+         *
+         * @param pool the pool to add
+         */
+        void addPool(LootPool pool);
+
+        /**
+         * Adds a pool to the loot table.
+         *
+         * @param pool the pool to add
+         */
+        default void addPool(LootPool.Builder pool) {
+            addPool(pool.build());
+        }
+    }
 
     public static void constructEntity(Entity entity) {
         if (entity instanceof Player player) {
@@ -37,8 +91,144 @@ public class WMCommonEventHandler {
         return EventResult.pass();
     }
 
+    public static LootPool.Builder getIronWeaponsLootPool() {
+        return addIronWeapons(LootPool.lootPool(), b -> b);
+    }
+
+    public static LootPool.Builder addIronWeapons(LootPool.Builder lootPool,
+                                                  Function<LootItem.Builder<?>, LootItem.Builder<?>> mod) {
+        return lootPool
+                .add(mod.apply(LootItem.lootTableItem(WMRegistries.ITEM_BATTLEAXE_IRON.get())
+                        .apply(SetItemCountFunction.setCount(ConstantValue.exactly(1)))))
+                .add(mod.apply(LootItem.lootTableItem(WMRegistries.ITEM_BOOMERANG_IRON.get())
+                        .apply(SetItemCountFunction.setCount(ConstantValue.exactly(1)))))
+                .add(mod.apply(LootItem.lootTableItem(WMRegistries.ITEM_FLAIL_IRON.get())
+                        .apply(SetItemCountFunction.setCount(ConstantValue.exactly(1)))))
+                .add(mod.apply(LootItem.lootTableItem(WMRegistries.ITEM_KATANA_IRON.get())
+                        .apply(SetItemCountFunction.setCount(ConstantValue.exactly(1)))))
+                .add(mod.apply(LootItem.lootTableItem(WMRegistries.ITEM_HALBERD_IRON.get())
+                        .apply(SetItemCountFunction.setCount(ConstantValue.exactly(1)))))
+                .add(mod.apply(LootItem.lootTableItem(WMRegistries.ITEM_KNIFE_IRON.get())
+                        .apply(SetItemCountFunction.setCount(ConstantValue.exactly(1)))))
+                .add(mod.apply(LootItem.lootTableItem(WMRegistries.ITEM_MUSKET_IRON.get())
+                        .apply(SetItemCountFunction.setCount(ConstantValue.exactly(1)))))
+                .add(mod.apply(LootItem.lootTableItem(WMRegistries.ITEM_SPEAR_IRON.get())
+                        .apply(SetItemCountFunction.setCount(ConstantValue.exactly(1)))))
+                .add(mod.apply(LootItem.lootTableItem(WMRegistries.ITEM_WARHAMMER_IRON.get())
+                        .apply(SetItemCountFunction.setCount(ConstantValue.exactly(1)))));
+    }
+
+    public static LootPool.Builder getGoldWeaponsLootPool() {
+        return addGoldWeapons(LootPool.lootPool(), b -> b);
+    }
+
+    public static LootPool.Builder addGoldWeapons(LootPool.Builder lootPool,
+                                                  Function<LootItem.Builder<?>, LootItem.Builder<?>> mod) {
+        return lootPool
+                .add(mod.apply(LootItem.lootTableItem(WMRegistries.ITEM_BATTLEAXE_GOLD.get())
+                        .apply(SetItemCountFunction.setCount(ConstantValue.exactly(1)))))
+                .add(mod.apply(LootItem.lootTableItem(WMRegistries.ITEM_BOOMERANG_GOLD.get())
+                        .apply(SetItemCountFunction.setCount(ConstantValue.exactly(1)))))
+                .add(mod.apply(LootItem.lootTableItem(WMRegistries.ITEM_FLAIL_GOLD.get())
+                        .apply(SetItemCountFunction.setCount(ConstantValue.exactly(1)))))
+                .add(mod.apply(LootItem.lootTableItem(WMRegistries.ITEM_KATANA_GOLD.get())
+                        .apply(SetItemCountFunction.setCount(ConstantValue.exactly(1)))))
+                .add(mod.apply(LootItem.lootTableItem(WMRegistries.ITEM_HALBERD_GOLD.get())
+                        .apply(SetItemCountFunction.setCount(ConstantValue.exactly(1)))))
+                .add(mod.apply(LootItem.lootTableItem(WMRegistries.ITEM_KNIFE_GOLD.get())
+                        .apply(SetItemCountFunction.setCount(ConstantValue.exactly(1)))))
+                .add(mod.apply(LootItem.lootTableItem(WMRegistries.ITEM_MUSKET_GOLD.get())
+                        .apply(SetItemCountFunction.setCount(ConstantValue.exactly(1)))))
+                .add(mod.apply(LootItem.lootTableItem(WMRegistries.ITEM_SPEAR_GOLD.get())
+                        .apply(SetItemCountFunction.setCount(ConstantValue.exactly(1)))))
+                .add(mod.apply(LootItem.lootTableItem(WMRegistries.ITEM_WARHAMMER_GOLD.get())
+                        .apply(SetItemCountFunction.setCount(ConstantValue.exactly(1)))));
+    }
+
+    public static LootPool.Builder getDiamondWeaponsLootPool() {
+        return addDiamondWeapons(LootPool.lootPool(), b -> b);
+    }
+
+    public static LootPool.Builder addDiamondWeapons(LootPool.Builder lootPool,
+                                                     Function<LootItem.Builder<?>, LootItem.Builder<?>> mod) {
+        return lootPool
+                .add(mod.apply(LootItem.lootTableItem(WMRegistries.ITEM_BATTLEAXE_DIAMOND.get())
+                        .apply(SetItemCountFunction.setCount(ConstantValue.exactly(1)))))
+                .add(mod.apply(LootItem.lootTableItem(WMRegistries.ITEM_BOOMERANG_DIAMOND.get())
+                        .apply(SetItemCountFunction.setCount(ConstantValue.exactly(1)))))
+                .add(mod.apply(LootItem.lootTableItem(WMRegistries.ITEM_FLAIL_DIAMOND.get())
+                        .apply(SetItemCountFunction.setCount(ConstantValue.exactly(1)))))
+                .add(mod.apply(LootItem.lootTableItem(WMRegistries.ITEM_KATANA_DIAMOND.get())
+                        .apply(SetItemCountFunction.setCount(ConstantValue.exactly(1)))))
+                .add(mod.apply(LootItem.lootTableItem(WMRegistries.ITEM_HALBERD_DIAMOND.get())
+                        .apply(SetItemCountFunction.setCount(ConstantValue.exactly(1)))))
+                .add(mod.apply(LootItem.lootTableItem(WMRegistries.ITEM_KNIFE_DIAMOND.get())
+                        .apply(SetItemCountFunction.setCount(ConstantValue.exactly(1)))))
+                .add(mod.apply(LootItem.lootTableItem(WMRegistries.ITEM_MUSKET_DIAMOND.get())
+                        .apply(SetItemCountFunction.setCount(ConstantValue.exactly(1)))))
+                .add(mod.apply(LootItem.lootTableItem(WMRegistries.ITEM_SPEAR_DIAMOND.get())
+                        .apply(SetItemCountFunction.setCount(ConstantValue.exactly(1)))))
+                .add(mod.apply(LootItem.lootTableItem(WMRegistries.ITEM_WARHAMMER_DIAMOND.get())
+                        .apply(SetItemCountFunction.setCount(ConstantValue.exactly(1)))));
+    }
+
+    public static void registerLootTableAdditions(LootTables lootTables, ResourceLocation id,
+                                                  LootTableModificationContext context, boolean builtIn) {
+        if (BuiltInLootTables.BASTION_BRIDGE.equals(id)) {
+            context.addPool(getGoldWeaponsLootPool().setRolls(UniformGenerator.between(0, 1)));
+        }
+        if (BuiltInLootTables.BASTION_OTHER.equals(id)) {
+            LootPool.Builder lootPool = LootPool.lootPool();
+            addGoldWeapons(lootPool, b -> b);
+            addIronWeapons(lootPool, b -> b.setWeight(2)
+                    .apply(SetItemDamageFunction.setDamage(UniformGenerator.between(0.1f, 0.9f)))
+                    .apply(EnchantRandomlyFunction.randomApplicableEnchantment())
+            );
+            context.addPool(lootPool.setRolls(UniformGenerator.between(0, 1)));
+        }
+        if (BuiltInLootTables.BASTION_TREASURE.equals(id)) {
+            LootPool.Builder lootPool = LootPool.lootPool();
+            addDiamondWeapons(lootPool, b -> b);
+            addDiamondWeapons(lootPool, b -> b
+                    .apply(SetItemDamageFunction.setDamage(UniformGenerator.between(0.8f, 1.0f)))
+                    .apply(EnchantRandomlyFunction.randomApplicableEnchantment())
+            );
+            context.addPool(lootPool.setRolls(UniformGenerator.between(0, 1)));
+        }
+        if (BuiltInLootTables.BURIED_TREASURE.equals(id)) {
+            context.addPool(getIronWeaponsLootPool().setRolls(UniformGenerator.between(0, 1)));
+        }
+        if (BuiltInLootTables.END_CITY_TREASURE.equals(id)) {
+            LootPool.Builder lootPool = LootPool.lootPool();
+            addDiamondWeapons(lootPool, b -> b
+                    .apply(EnchantWithLevelsFunction.enchantWithLevels(UniformGenerator.between(20, 39)))
+            );
+            addIronWeapons(lootPool, b -> b
+                    .apply(EnchantWithLevelsFunction.enchantWithLevels(UniformGenerator.between(20, 39)))
+            );
+            context.addPool(lootPool.setRolls(UniformGenerator.between(0, 1)));
+        }
+        if (BuiltInLootTables.NETHER_BRIDGE.equals(id)) {
+            context.addPool(getGoldWeaponsLootPool().setRolls(UniformGenerator.between(0, 1)));
+        }
+        if (BuiltInLootTables.RUINED_PORTAL.equals(id)) {
+            LootPool.Builder lootPool = LootPool.lootPool();
+            addGoldWeapons(lootPool, b -> b
+                    .apply(EnchantRandomlyFunction.randomApplicableEnchantment())
+            );
+            context.addPool(lootPool.setRolls(UniformGenerator.between(0, 1)));
+        }
+        if (BuiltInLootTables.STRONGHOLD_CORRIDOR.equals(id)) {
+            context.addPool(getIronWeaponsLootPool().setRolls(UniformGenerator.between(0, 1)));
+        }
+        if (BuiltInLootTables.VILLAGE_WEAPONSMITH.equals(id)) {
+            context.addPool(getIronWeaponsLootPool().setRolls(UniformGenerator.between(0, 1)));
+        }
+    }
+
     public static void init() {
         EntityEvent.LIVING_HURT.register(WMCommonEventHandler::cancelBlockingOfRangedWeapons);
+        MODIFY_LOOT_TABLE.register(WMCommonEventHandler::registerLootTableAdditions);
     }
 
 }
--- forge/src/main/java/ckathode/weaponmod/forge/WMCommonEventHandlerForge.java
+++ forge/src/main/java/ckathode/weaponmod/forge/WMCommonEventHandlerForge.java
@@ -1,6 +1,7 @@
 package ckathode.weaponmod.forge;
 
 import ckathode.weaponmod.WMCommonEventHandler;
+import net.minecraftforge.event.LootTableLoadEvent;
 import net.minecraftforge.event.entity.EntityEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 
@@ -9,6 +10,12 @@ public class WMCommonEventHandlerForge {
     @SubscribeEvent
     public void constructEntity(EntityEvent.EntityConstructing event) {
         WMCommonEventHandler.constructEntity(event.getEntity());
+    }
+
+    @SubscribeEvent
+    public void registerLootTableAdditions(LootTableLoadEvent event) {
+        WMCommonEventHandler.MODIFY_LOOT_TABLE.invoker().modifyLootTable(event.getLootTableManager(), event.getName(),
+                (pool) -> event.getTable().addPool(pool), true);
     }
 
 }
