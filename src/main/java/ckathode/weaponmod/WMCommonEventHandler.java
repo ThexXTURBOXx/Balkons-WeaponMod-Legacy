297efea298a7773629382076a384cb74a0fafd28
Nico Mexis <nico.mexis@kabelmail.de>
2026-07-17 14:34:59 +0200

    Fix firearms stop firing even more
    Fixes #124

--- src/main/java/ckathode/weaponmod/WMCommonEventHandler.java
+++ src/main/java/ckathode/weaponmod/WMCommonEventHandler.java
@@ -7,8 +7,10 @@ import ckathode.weaponmod.item.WMItem;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.MobEffects;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.DamageSource;
 import net.minecraft.util.math.MathHelper;
 import net.minecraftforge.event.entity.EntityEvent;
 import net.minecraftforge.event.entity.living.LivingAttackEvent;
@@ -44,11 +46,20 @@ public class WMCommonEventHandler {
     @SubscribeEvent
     public void cancelBlockingOfRangedWeapons(LivingAttackEvent event) {
         EntityLivingBase entity = event.getEntityLiving();
+        DamageSource source = event.getSource();
+        float amount = event.getAmount();
+
         ItemStack stack = entity.getActiveItemStack();
         Item item = stack == null ? null : stack.getItem();
+        if (Float.isFinite(amount) && amount <= 0) return;
         if (!(item instanceof IItemWeapon)) return;
-        if (entity instanceof EntityPlayer && ((EntityPlayer) entity).isCreative() &&
-            !event.getSource().canHarmInCreative()) return;
+        if (entity.isEntityInvulnerable(source)) return;
+        if (entity.getHealth() <= 0) return;
+        if (source.isFireDamage() && entity.isPotionActive(MobEffects.FIRE_RESISTANCE)) return;
+        if (entity instanceof EntityPlayer) {
+            EntityPlayer player = (EntityPlayer) entity;
+            if (player.capabilities.disableDamage && !source.canHarmInCreative()) return;
+        }
 
         entity.resetActiveHand();
     }
