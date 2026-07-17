f8325ed744e8f1b426e1be4c9eeaca65bdfae3e6
Nico Mexis <nico.mexis@kabelmail.de>
2026-07-17 07:37:01 +0200

    Instakill projectiles when weapon is disabled in config

--- common/src/main/java/ckathode/weaponmod/entity/projectile/EntityMortarShell.java
+++ common/src/main/java/ckathode/weaponmod/entity/projectile/EntityMortarShell.java
@@ -4,11 +4,8 @@ import ckathode.weaponmod.PhysHelper;
 import ckathode.weaponmod.WMDamageSources;
 import ckathode.weaponmod.WMRegistries;
 import ckathode.weaponmod.WeaponModConfig;
-import dev.architectury.networking.NetworkManager;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.particles.ParticleTypes;
-import net.minecraft.network.protocol.Packet;
-import net.minecraft.network.protocol.game.ClientGamePacketListener;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.util.Mth;
 import net.minecraft.world.damagesource.DamageSource;
@@ -49,10 +46,9 @@ public class EntityMortarShell extends EntityProjectile<EntityMortarShell> {
         setPickupStatusFromEntity(shooter);
     }
 
-    @NotNull
     @Override
-    public Packet<ClientGamePacketListener> getAddEntityPacket() {
-        return NetworkManager.createAddEntityPacket(this);
+    protected boolean isDisabled() {
+        return !WeaponModConfig.get().isEnabled("mortar");
     }
 
     @Override
