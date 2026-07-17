aa8d6a2e9a1f21c5d8ec3e854d668dd5995b7398
Nico Mexis <nico.mexis@kabelmail.de>
2026-07-17 14:33:45 +0200

    Instakill projectiles when weapon is disabled in config

--- src/main/java/ckathode/weaponmod/entity/EntityDummy.java
+++ src/main/java/ckathode/weaponmod/entity/EntityDummy.java
@@ -141,6 +141,10 @@ public class EntityDummy extends Entity {
 
     @Override
     public void onUpdate() {
+        if (!BalkonsWeaponMod.instance.modConfig.isEnabled("dummy")) {
+            setDead();
+            return;
+        }
         super.onUpdate();
         int i = getTimeSinceHit();
         if (i > 0) {
--- src/main/java/ckathode/weaponmod/entity/projectile/EntityFlail.java
+++ src/main/java/ckathode/weaponmod/entity/projectile/EntityFlail.java
@@ -1,5 +1,6 @@
 package ckathode.weaponmod.entity.projectile;
 
+import ckathode.weaponmod.BalkonsWeaponMod;
 import ckathode.weaponmod.PlayerWeaponData;
 import ckathode.weaponmod.WeaponDamageSource;
 import ckathode.weaponmod.item.ItemFlail;
@@ -46,6 +47,11 @@ public class EntityFlail extends EntityMaterialProjectile {
         setPickupStatusFromEntity(shooter);
         setThrownItemStack(itemstack);
         distanceTotal = 0.0;
+    }
+
+    @Override
+    protected boolean isDisabled() {
+        return !BalkonsWeaponMod.instance.modConfig.isEnabled("flail");
     }
 
     @Override
