9f0457b7c97de1def0402e7ddbe0b9bb5e3e418c
Nico Mexis <nico.mexis@kabelmail.de>
2026-07-17 14:33:45 +0200

    Instakill projectiles when weapon is disabled in config

--- src/main/java/ckathode/weaponmod/entity/projectile/EntitySpear.java
+++ src/main/java/ckathode/weaponmod/entity/projectile/EntitySpear.java
@@ -1,5 +1,6 @@
 package ckathode.weaponmod.entity.projectile;
 
+import ckathode.weaponmod.BalkonsWeaponMod;
 import ckathode.weaponmod.WeaponDamageSource;
 import ckathode.weaponmod.item.IItemWeapon;
 import net.minecraft.entity.Entity;
@@ -29,6 +30,11 @@ public class EntitySpear extends EntityMaterialProjectile {
         setThrower(shooter);
         setPickupStatusFromEntity(shooter);
         setThrownItemStack(itemstack);
+    }
+
+    @Override
+    protected boolean isDisabled() {
+        return !BalkonsWeaponMod.instance.modConfig.isEnabled("spear");
     }
 
     @Override
