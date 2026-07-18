cec32accf7cdbd1ad2e0feb10651803f2dffc16e
Nico Mexis <nico.mexis@kabelmail.de>
2026-07-18 23:18:52 +0200

    Disable loot table entries if disabled in config

---
+++ common/src/main/java/ckathode/weaponmod/WMLootConfigCondition.java
@@ -0,0 +1,37 @@
+package ckathode.weaponmod;
+
+import com.mojang.serialization.Codec;
+import com.mojang.serialization.MapCodec;
+import com.mojang.serialization.codecs.RecordCodecBuilder;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import net.minecraft.world.level.storage.loot.LootContext;
+import net.minecraft.world.level.storage.loot.predicates.LootItemCondition;
+import org.jetbrains.annotations.NotNull;
+
+public record WMLootConfigCondition(List<String> weapons) implements LootItemCondition {
+
+    public static final String ID = "loot_config_condition";
+    public static final MapCodec<WMLootConfigCondition> CODEC =
+            RecordCodecBuilder.mapCodec((instance) ->
+                    instance.group(Codec.STRING.listOf().fieldOf("weapons").forGetter(WMLootConfigCondition::weapons)).apply(instance, WMLootConfigCondition::new));
+
+    @Override
+    public boolean test(LootContext lootContext) {
+        return weapons.stream().allMatch(cfg -> WeaponModConfig.get().isEnabled(cfg));
+    }
+
+    @NotNull
+    @Override
+    public MapCodec<? extends LootItemCondition> codec() {
+        return CODEC;
+    }
+
+    public static LootItemCondition.Builder of(String... weapons) {
+        List<String> list = new ArrayList<>(weapons.length);
+        Collections.addAll(list, weapons);
+        return () -> new WMLootConfigCondition(list);
+    }
+
+}
