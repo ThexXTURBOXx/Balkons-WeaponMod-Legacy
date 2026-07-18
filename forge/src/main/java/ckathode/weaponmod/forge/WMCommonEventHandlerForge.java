9b29f0c7cd41396ed17564473af94fad8c196e1f
Nico Mexis <nico.mexis@kabelmail.de>
2026-07-18 23:13:55 +0200

    Allow zombies to sometimes spawn with weapons

--- forge/src/main/java/ckathode/weaponmod/forge/WMCommonEventHandlerForge.java
+++ forge/src/main/java/ckathode/weaponmod/forge/WMCommonEventHandlerForge.java
@@ -2,6 +2,7 @@ package ckathode.weaponmod.forge;
 
 import ckathode.weaponmod.WMCommonEventHandler;
 import net.minecraftforge.event.entity.EntityEvent;
+import net.minecraftforge.event.entity.living.LivingSpawnEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 
 public class WMCommonEventHandlerForge {
@@ -9,6 +10,11 @@ public class WMCommonEventHandlerForge {
     @SubscribeEvent
     public void constructEntity(EntityEvent.EntityConstructing event) {
         WMCommonEventHandler.constructEntity(event.getEntity());
+    }
+
+    @SubscribeEvent
+    public void onEntitySpawn(LivingSpawnEvent.SpecialSpawn event) {
+        WMCommonEventHandler.equipZombies(event.getEntity(), event.getLevel());
     }
 
 }
