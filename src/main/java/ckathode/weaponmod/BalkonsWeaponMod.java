6e0e9ab08c5be2d97ed44e015a974513ec9d729e
Nico Mexis <nico.mexis@kabelmail.de>
2026-07-18 14:02:48 +0200

    Allow zombies to sometimes spawn with weapons

--- src/main/java/ckathode/weaponmod/BalkonsWeaponMod.java
+++ src/main/java/ckathode/weaponmod/BalkonsWeaponMod.java
@@ -196,6 +196,7 @@ public class BalkonsWeaponMod {
         registerRecipes();
         registerDispenseBehavior();
         registerLootTableEntries();
+        registerZombieWeapons();
     }
 
     @Mod.EventHandler
@@ -615,6 +616,16 @@ public class BalkonsWeaponMod {
             ChestGenHooks.addItem(ChestGenHooks.NETHER_FORTRESS,
                     new WeightedRandomChestContent(goldItems[i], 0, 1, 1, 5));
         }
+    }
+
+    private void registerZombieWeapons() {
+        WMCommonEventHandler.registerZombieWeapon(battleaxeSteel, "battleaxe");
+        WMCommonEventHandler.registerZombieWeapon(katanaSteel, "katana");
+        WMCommonEventHandler.registerZombieWeapon(halberdSteel, "halberd");
+        WMCommonEventHandler.registerZombieWeapon(knifeSteel, "knife");
+        WMCommonEventHandler.registerZombieWeapon(spearSteel, "spear");
+        WMCommonEventHandler.registerZombieWeapon(warhammerSteel, "warhammer");
+        WMCommonEventHandler.registerZombieWeapon(fireRod, "firerod");
     }
 
 }
