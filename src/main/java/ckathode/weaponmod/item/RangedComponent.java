d96421ec1545d4e531c3c59e610f2e089f333ee5
Nico Mexis <nico.mexis@kabelmail.de>
2026-07-19 09:41:53 +0200

    Utilise more item tags and make them accessible in code

--- src/main/java/ckathode/weaponmod/item/RangedComponent.java
+++ src/main/java/ckathode/weaponmod/item/RangedComponent.java
@@ -3,13 +3,10 @@ package ckathode.weaponmod.item;
 import ckathode.weaponmod.BalkonsWeaponMod;
 import ckathode.weaponmod.ReloadHelper;
 import ckathode.weaponmod.ReloadHelper.ReloadState;
+import ckathode.weaponmod.WMItemTags;
 import ckathode.weaponmod.WeaponModAttributes;
 import ckathode.weaponmod.entity.projectile.EntityProjectile;
 import com.google.common.collect.Multimap;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Objects;
-import java.util.stream.Collectors;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.entity.Entity;
@@ -22,16 +19,15 @@ import net.minecraft.item.EnumAction;
 import net.minecraft.item.Item;
 import net.minecraft.item.Item.Properties;
 import net.minecraft.item.ItemStack;
+import net.minecraft.tags.Tag;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.EnumActionResult;
 import net.minecraft.util.EnumHand;
 import net.minecraft.util.NonNullList;
-import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.SoundCategory;
 import net.minecraft.util.Tuple;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
-import net.minecraftforge.registries.ForgeRegistries;
 
 public abstract class RangedComponent extends AbstractWeaponComponent {
     protected static final int MAX_DELAY = 72000;
@@ -233,12 +229,12 @@ public abstract class RangedComponent extends AbstractWeaponComponent {
         return rangedSpecs.getReloadTime();
     }
 
-    public List<Item> getAmmoItems() {
-        return rangedSpecs.getAmmoItems();
+    public Tag<Item> getAmmoTag() {
+        return rangedSpecs.getAmmoTag();
     }
 
     protected ItemStack findAmmo(EntityPlayer entityplayer) {
-        Tuple<EnumHand, Integer> slot = WMItem.findAnyItemSlot(entityplayer, getAmmoItems());
+        Tuple<EnumHand, Integer> slot = WMItem.findAnyItemSlot(entityplayer, getAmmoTag());
         if (slot == null) return ItemStack.EMPTY;
         NonNullList<ItemStack> inv = slot.getA() == EnumHand.OFF_HAND ? entityplayer.inventory.offHandInventory :
                 entityplayer.inventory.mainInventory;
@@ -246,7 +242,7 @@ public abstract class RangedComponent extends AbstractWeaponComponent {
     }
 
     protected boolean consumeAmmo(EntityPlayer entityplayer) {
-        return WMItem.consumeAnyInventoryItem(entityplayer, getAmmoItems());
+        return WMItem.consumeAnyInventoryItem(entityplayer, getAmmoTag());
     }
 
     public boolean hasAmmoAndConsume(ItemStack itemstack, World world, EntityLivingBase entityliving) {
@@ -281,26 +277,22 @@ public abstract class RangedComponent extends AbstractWeaponComponent {
     }
 
     public enum RangedSpecs {
-        BLOWGUN("blowgun", 250, DartType.DART_TYPES.stream()
-                .map(t -> BalkonsWeaponMod.id(t.typeName).toString())
-                .toArray(String[]::new)),
-        CROSSBOW("crossbow", 250, BalkonsWeaponMod.id("bolt").toString()),
-        MUSKET("musket", 80, BalkonsWeaponMod.id("bullet").toString()),
-        BLUNDERBUSS("blunderbuss", 80, BalkonsWeaponMod.id("shot").toString()),
-        FLINTLOCK("flintlock", 8, BalkonsWeaponMod.id("bullet").toString()),
-        MORTAR("mortar", 40, BalkonsWeaponMod.id("shell").toString());
+        BLOWGUN("blowgun", 250, WMItemTags.DARTS),
+        CROSSBOW("crossbow", 250, WMItemTags.BOLTS),
+        MUSKET("musket", 80, WMItemTags.BULLETS),
+        BLUNDERBUSS("blunderbuss", 80, WMItemTags.SHOTS),
+        FLINTLOCK("flintlock", 8, WMItemTags.BULLETS),
+        MORTAR("mortar", 40, WMItemTags.SHELLS);
 
         private int reloadTime;
-        private List<Item> ammoItems;
-        private final String[] ammoItemTags;
+        private final Tag<Item> ammoTag;
         public final String reloadTimeTag;
         public final int durability;
 
-        RangedSpecs(String reloadtimetag, int durability, String... ammoitemtags) {
-            ammoItemTags = ammoitemtags;
+        RangedSpecs(String reloadtimetag, int durability, Tag<Item> ammoTag) {
             reloadTimeTag = reloadtimetag;
             this.durability = durability;
-            ammoItems = null;
+            this.ammoTag = ammoTag;
             reloadTime = -1;
         }
 
@@ -313,16 +305,8 @@ public abstract class RangedComponent extends AbstractWeaponComponent {
             return reloadTime;
         }
 
-        public List<Item> getAmmoItems() {
-            if (ammoItems == null) {
-                ammoItems = Arrays.stream(ammoItemTags)
-                        .map(t -> ForgeRegistries.ITEMS.getValue(new ResourceLocation(t)))
-                        .filter(Objects::nonNull)
-                        .collect(Collectors.toList());
-                BalkonsWeaponMod.modLog.debug("Found items {} for {} @{}",
-                        ammoItems, Arrays.toString(ammoItemTags), this);
-            }
-            return ammoItems;
+        public Tag<Item> getAmmoTag() {
+            return ammoTag;
         }
     }
 }
--- src/main/java/ckathode/weaponmod/item/WMItem.java
+++ src/main/java/ckathode/weaponmod/item/WMItem.java
@@ -4,11 +4,13 @@ import ckathode.weaponmod.BalkonsWeaponMod;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.UUID;
+import java.util.function.Predicate;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
+import net.minecraft.tags.Tag;
 import net.minecraft.util.EnumHand;
 import net.minecraft.util.NonNullList;
 import net.minecraft.util.ResourceLocation;
@@ -52,19 +54,33 @@ public class WMItem extends Item {
         }
     }
 
+    public static boolean isItemInTag(ItemStack stack, Tag<Item> tag) {
+        return stack != null && tag.contains(stack.getItem());
+    }
+
     public static boolean isItemInList(ItemStack stack, Collection<Item> items) {
         return stack != null && items.contains(stack.getItem());
     }
 
     @Nullable
-    public static Tuple<EnumHand, Integer> findAnyItemSlot(EntityPlayer player, Collection<Item> item) {
-        if (isItemInList(player.getHeldItemMainhand(), item))
+    public static Tuple<EnumHand, Integer> findAnyItemSlot(EntityPlayer player, Tag<Item> tag) {
+        return findAnyItemSlot(player, is -> isItemInTag(is, tag));
+    }
+
+    @Nullable
+    public static Tuple<EnumHand, Integer> findAnyItemSlot(EntityPlayer player, Collection<Item> items) {
+        return findAnyItemSlot(player, is -> isItemInList(is, items));
+    }
+
+    @Nullable
+    public static Tuple<EnumHand, Integer> findAnyItemSlot(EntityPlayer player, Predicate<ItemStack> matcher) {
+        if (matcher.test(player.getHeldItemMainhand()))
             return new Tuple<>(EnumHand.MAIN_HAND, player.inventory.currentItem);
-        if (isItemInList(player.getHeldItemOffhand(), item))
+        if (matcher.test(player.getHeldItemOffhand()))
             return new Tuple<>(EnumHand.OFF_HAND, 0);
         for (int i = 0; i < player.inventory.getSizeInventory(); ++i) {
             ItemStack itemstack = player.inventory.getStackInSlot(i);
-            if (isItemInList(itemstack, item))
+            if (matcher.test(itemstack))
                 return new Tuple<>(EnumHand.MAIN_HAND, i);
         }
         return null;
@@ -74,8 +90,26 @@ public class WMItem extends Item {
         return consumeAnyInventoryItem(player, Collections.singletonList(item));
     }
 
-    public static boolean consumeAnyInventoryItem(EntityPlayer player, Collection<Item> item) {
+    public static boolean consumeAnyInventoryItem(EntityPlayer player, Tag<Item> item) {
         Tuple<EnumHand, Integer> slot = findAnyItemSlot(player, item);
+        if (slot == null) return false;
+        NonNullList<ItemStack> inv = slot.getA() == EnumHand.OFF_HAND ? player.inventory.offHandInventory :
+                player.inventory.mainInventory;
+        inv.get(slot.getB()).shrink(1);
+        return true;
+    }
+
+    public static boolean consumeAnyInventoryItem(EntityPlayer player, Collection<Item> items) {
+        Tuple<EnumHand, Integer> slot = findAnyItemSlot(player, items);
+        if (slot == null) return false;
+        NonNullList<ItemStack> inv = slot.getA() == EnumHand.OFF_HAND ? player.inventory.offHandInventory :
+                player.inventory.mainInventory;
+        inv.get(slot.getB()).shrink(1);
+        return true;
+    }
+
+    public static boolean consumeAnyInventoryItem(EntityPlayer player, Predicate<ItemStack> matcher) {
+        Tuple<EnumHand, Integer> slot = findAnyItemSlot(player, matcher);
         if (slot == null) return false;
         NonNullList<ItemStack> inv = slot.getA() == EnumHand.OFF_HAND ? player.inventory.offHandInventory :
                 player.inventory.mainInventory;
