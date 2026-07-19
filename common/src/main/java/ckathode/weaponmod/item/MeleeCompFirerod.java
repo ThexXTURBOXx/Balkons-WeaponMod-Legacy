906c578626d65203b78df4705b7bff3f1f911220
Nico Mexis <nico.mexis@kabelmail.de>
2026-07-19 12:02:40 +0200

    Fix a few remaining issues

--- common/src/main/java/ckathode/weaponmod/item/MeleeCompFirerod.java
+++ common/src/main/java/ckathode/weaponmod/item/MeleeCompFirerod.java
@@ -32,11 +32,11 @@ public class MeleeCompFirerod extends MeleeComponent {
     }
 
     public void applyFire(LivingEntity entity, ItemStack stack) {
-        entity.setSecondsOnFire(Math.max(entity.getRemainingFireTicks(), 0) + 12 +
+        entity.setSecondsOnFire(Math.max(entity.getRemainingFireTicks() / 20, 0) + 12 +
                                 2 * EnchantmentHelper.getItemEnchantmentLevel(Enchantments.FIRE_ASPECT, stack) +
                                 entity.getRandom().nextInt(3));
-        if (entity instanceof Creeper && !entity.level().isClientSide())
-            ((Creeper) entity).ignite();
+        if (entity instanceof Creeper creeper && !entity.level().isClientSide())
+            creeper.ignite();
     }
 
     @Override
