b2836be1e1ebac72c984cece40b2dd59a03c3f0d
Nico Mexis <nico.mexis@kabelmail.de>
2026-07-19 12:54:53 +0200

    Remove some dependencies to LWJGL

--- common/src/main/java/ckathode/weaponmod/render/RenderBoomerang.java
+++ common/src/main/java/ckathode/weaponmod/render/RenderBoomerang.java
@@ -3,7 +3,6 @@ package ckathode.weaponmod.render;
 import ckathode.weaponmod.WeaponModConfig;
 import ckathode.weaponmod.WeaponModResources;
 import ckathode.weaponmod.entity.projectile.EntityBoomerang;
-import com.mojang.blaze3d.platform.GlStateManager;
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.blaze3d.vertex.VertexConsumer;
 import com.mojang.math.Axis;
@@ -64,7 +63,6 @@ public class RenderBoomerang extends WMRenderer<EntityBoomerang> {
                 drawVertex(last, builder, 0.0f, 0.0f, 0.0f, color[0], color[1], color[2], 1.0f, 0.5f, 0.0f, 0.0f,
                         -1.0f, 0.0f, lm);
             }
-            GlStateManager._disableCull();
             drawVertex(last, builder, 0.2f, -0.08f, 0.8f, 0.5f, 0.5f, -SQRT2, 0.0f, SQRT2, lm);
             drawVertex(last, builder, 0.2f, 0.08f, 0.8f, 0.5f, 0.65625f, -SQRT2, 0.0f, SQRT2, lm);
             drawVertex(last, builder, 0.9f, 0.08f, 0.8f, 0.0f, 0.65625f, -SQRT2, 0.0f, SQRT2, lm);
@@ -93,7 +91,6 @@ public class RenderBoomerang extends WMRenderer<EntityBoomerang> {
                 drawVertex(last, builder, 0.2f, -0.08f, 0.2f, color[0], color[1], color[2], 1.0f, 0.5f, 0.5f, -SQRT2,
                         0.0f, SQRT2, lm);
             }
-            GlStateManager._enableCull();
             ms.popPose();
         } else {
             ItemRenderer itemRender = Minecraft.getInstance().getItemRenderer();
