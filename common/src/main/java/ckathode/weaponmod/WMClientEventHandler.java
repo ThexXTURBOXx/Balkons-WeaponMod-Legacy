739d4377f280ab8a31f969de28ab18b93db2112a
Nico Mexis <nico.mexis@kabelmail.de>
2026-07-19 13:04:52 +0200

    Remove some dependencies to LWJGL

--- common/src/main/java/ckathode/weaponmod/WMClientEventHandler.java
+++ common/src/main/java/ckathode/weaponmod/WMClientEventHandler.java
@@ -7,6 +7,7 @@ import ckathode.weaponmod.item.IItemWeapon;
 import ckathode.weaponmod.item.RangedComponent;
 import ckathode.weaponmod.network.MsgCannonFire;
 import ckathode.weaponmod.network.WMMessagePipeline;
+import com.mojang.blaze3d.platform.InputConstants;
 import dev.architectury.event.EventResult;
 import dev.architectury.event.events.client.ClientRawInputEvent;
 import dev.architectury.event.events.client.ClientTooltipEvent;
@@ -27,7 +28,6 @@ import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.phys.EntityHitResult;
 import net.minecraft.world.phys.HitResult;
 import org.jetbrains.annotations.Nullable;
-import org.lwjgl.glfw.GLFW;
 
 public class WMClientEventHandler {
 
@@ -48,7 +48,7 @@ public class WMClientEventHandler {
         if (player == null || !player.level().isClientSide() || mc.gui.screen() != null) {
             return EventResult.pass();
         }
-        if (mouseButtonInfo.button() == 0 && action == GLFW.GLFW_PRESS) {
+        if (mouseButtonInfo.button() == 0 && action == InputConstants.PRESS) {
             ItemStack itemstack = player.getMainHandItem();
             if (!itemstack.isEmpty()) {
                 IExtendedReachItem ieri = getExtendedReachItem(itemstack);
