fdb02ff17b1192092ad4a4ca1b7b0e4263cb8304
Nico Mexis <nico.mexis@kabelmail.de>
2026-07-19 08:17:54 +0200

    Firerods now extend fire periods and ignite Creepers

--- common/src/main/java/ckathode/weaponmod/WMCommonEventHandler.java
+++ common/src/main/java/ckathode/weaponmod/WMCommonEventHandler.java
@@ -1,6 +1,7 @@
 package ckathode.weaponmod;
 
 import ckathode.weaponmod.item.IItemWeapon;
+import ckathode.weaponmod.item.MeleeCompFirerod;
 import dev.architectury.event.EventResult;
 import dev.architectury.event.events.common.EntityEvent;
 import dev.architectury.event.events.common.LootEvent;
@@ -39,6 +40,18 @@ public class WMCommonEventHandler {
         if (entity instanceof Player player) {
             PlayerWeaponData.initPlayerWeaponData(player, builder);
         }
+    }
+
+    public static EventResult onEntityAttack(LivingEntity entity, DamageSource damageSource, float amount) {
+        Entity source = damageSource.getEntity();
+        if (!(source instanceof LivingEntity living)) return EventResult.pass();
+
+        ItemStack stack = living.getMainHandItem();
+        if (stack.is(MeleeCompFirerod.ITEM)) {
+            ((MeleeCompFirerod) MeleeCompFirerod.ITEM.meleeComponent).applyFire(entity, stack);
+        }
+
+        return EventResult.pass();
     }
 
     public static EventResult cancelBlockingOfRangedWeapons(LivingEntity entity, DamageSource source, float amount) {
@@ -247,6 +260,7 @@ public class WMCommonEventHandler {
 
     public static void init() {
         EntityEvent.LIVING_HURT.register(WMCommonEventHandler::cancelBlockingOfRangedWeapons);
+        EntityEvent.LIVING_HURT.register(WMCommonEventHandler::onEntityAttack);
         LootEvent.MODIFY_LOOT_TABLE.register(WMCommonEventHandler::registerLootTableAdditions);
     }
 
