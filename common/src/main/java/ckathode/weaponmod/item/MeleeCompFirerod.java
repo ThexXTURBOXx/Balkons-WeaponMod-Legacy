c12427ec107dce0ef54254f3413a7f3e2a4f40d2
Nico Mexis <nico.mexis@kabelmail.de>
2026-07-19 08:14:45 +0200

    Firerods now extend fire periods and ignite Creepers

--- common/src/main/java/ckathode/weaponmod/item/MeleeCompFirerod.java
+++ common/src/main/java/ckathode/weaponmod/item/MeleeCompFirerod.java
@@ -8,6 +8,7 @@ import net.minecraft.util.Mth;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.HumanoidArm;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.monster.Creeper;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Tiers;
@@ -29,15 +30,18 @@ public class MeleeCompFirerod extends MeleeComponent {
     @Override
     public boolean hurtEnemy(ItemStack itemstack, LivingEntity entityliving, LivingEntity attacker) {
         boolean flag = super.hurtEnemy(itemstack, entityliving, attacker);
-        if (flag) {
-            Holder<Enchantment> fireAspect = entityliving.registryAccess()
-                    .registryOrThrow(Registries.ENCHANTMENT).getHolder(Enchantments.FIRE_ASPECT).orElse(null);
-            int enchBonus = fireAspect != null
-                    ? 2 * EnchantmentHelper.getItemEnchantmentLevel(fireAspect, itemstack) : 0;
-            entityliving.igniteForSeconds(12 + enchBonus +
-                                          entityliving.getRandom().nextInt(3));
-        }
+        if (flag) applyFire(entityliving, itemstack);
         return flag;
+    }
+
+    public void applyFire(LivingEntity entity, ItemStack stack) {
+        Holder<Enchantment> fireAspect = entity.registryAccess()
+                .registryOrThrow(Registries.ENCHANTMENT).getHolder(Enchantments.FIRE_ASPECT).orElse(null);
+        int enchBonus = fireAspect != null ? 2 * EnchantmentHelper.getItemEnchantmentLevel(fireAspect, stack) : 0;
+        entity.igniteForSeconds(Math.max(entity.getRemainingFireTicks(), 0) + 12 + enchBonus +
+                                entity.getRandom().nextInt(3));
+        if (entity instanceof Creeper && !entity.level().isClientSide())
+            ((Creeper) entity).ignite();
     }
 
     @Override
