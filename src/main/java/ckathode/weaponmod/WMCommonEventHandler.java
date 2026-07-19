bcf0cc22077c9c176fc94193ae212978f3e3f227
Nico Mexis <nico.mexis@kabelmail.de>
2026-07-19 09:03:38 +0200

    Fix a few remaining issues

--- src/main/java/ckathode/weaponmod/WMCommonEventHandler.java
+++ src/main/java/ckathode/weaponmod/WMCommonEventHandler.java
@@ -57,7 +57,6 @@ public class WMCommonEventHandler {
 
         EntityLivingBase living = (EntityLivingBase) source;
         ItemStack stack = living.getHeldItemMainhand();
-        if (stack.isEmpty()) return;
         Item item = stack.getItem();
         if (item == BalkonsWeaponMod.fireRod) {
             ((MeleeCompFirerod) BalkonsWeaponMod.fireRod.meleeComponent).applyFire(event.getEntityLiving(), stack);
@@ -74,7 +73,7 @@ public class WMCommonEventHandler {
         if (entity.rand.nextFloat() < (event.getWorld().getDifficulty() == EnumDifficulty.HARD ? 0.05F : 0.01F)) {
             ZOMBIE_WEAPONS.entrySet()
                     .stream()
-                    .skip(entity.rand.nextInt(ZOMBIE_WEAPONS.size()))
+                    .skip(entity.rand.nextInt(Math.max(1, ZOMBIE_WEAPONS.size())))
                     .findFirst()
                     .ifPresent(e -> {
                         if (e.getValue().stream().anyMatch(cfg ->
@@ -129,8 +128,10 @@ public class WMCommonEventHandler {
     }
 
     private static LootCondition cfgCondition(String... configs) {
-        return (rand, context) -> Arrays.stream(configs).allMatch(cfg ->
-                BalkonsWeaponMod.instance.modConfig.isEnabled(cfg));
+        return (rand, context) ->
+                BalkonsWeaponMod.instance.modConfig.enableLootTables &&
+                Arrays.stream(configs).allMatch(cfg ->
+                        BalkonsWeaponMod.instance.modConfig.isEnabled(cfg));
     }
 
     public static LootPool addIronWeapons(LootPool lootPool, LootFunction... lootFunctions) {
@@ -228,8 +229,6 @@ public class WMCommonEventHandler {
 
     @SubscribeEvent
     public void registerLootTableAdditions(LootTableLoadEvent event) {
-        if (!BalkonsWeaponMod.instance.modConfig.enableLootTables) return;
-
         ResourceLocation id = event.getName();
         LootTable lootTable = event.getTable();
 
