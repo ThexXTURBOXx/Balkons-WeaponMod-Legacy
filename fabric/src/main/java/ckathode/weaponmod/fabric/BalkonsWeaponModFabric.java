90069e936b10d55aa33b4b725f6320306eb58366
Nico Mexis <nico.mexis@kabelmail.de>
2026-07-20 20:55:15 +0200

    Enable loot tables on Fabric using old API

--- fabric/src/main/java/ckathode/weaponmod/fabric/BalkonsWeaponModFabric.java
+++ fabric/src/main/java/ckathode/weaponmod/fabric/BalkonsWeaponModFabric.java
@@ -1,13 +1,18 @@
 package ckathode.weaponmod.fabric;
 
 import ckathode.weaponmod.BalkonsWeaponMod;
+import ckathode.weaponmod.WMCommonEventHandler;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.loot.v1.event.LootTableLoadingCallback;
 
 public class BalkonsWeaponModFabric implements ModInitializer {
 
     @Override
     public void onInitialize() {
         BalkonsWeaponMod.init();
+
+        LootTableLoadingCallback.EVENT.register((resourceManager, lootManager, id, supplier, setter) ->
+                WMCommonEventHandler.MODIFY_LOOT_TABLE.invoker().modifyLootTable(lootManager, id, supplier::withPool));
     }
 
 }
