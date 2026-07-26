fa485c66b2f691517d68c7e80bbefdcd821ecbd9
Nico Mexis <nico.mexis@kabelmail.de>
2026-07-26 20:35:12 +0200

    Add workaround for incomplete loot table events

--- fabric/src/main/java/ckathode/weaponmod/fabric/BalkonsWeaponModFabric.java
+++ fabric/src/main/java/ckathode/weaponmod/fabric/BalkonsWeaponModFabric.java
@@ -1,7 +1,14 @@
 package ckathode.weaponmod.fabric;
 
 import ckathode.weaponmod.BalkonsWeaponMod;
+import ckathode.weaponmod.WMCommonEventHandler;
+import dev.architectury.event.events.common.LootEvent;
+import java.lang.invoke.MethodHandle;
+import java.lang.invoke.MethodHandles;
+import java.lang.reflect.Constructor;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.loot.v3.LootTableEvents;
+import net.minecraft.world.level.storage.loot.LootTable;
 
 public class BalkonsWeaponModFabric implements ModInitializer {
 
@@ -9,6 +16,27 @@ public class BalkonsWeaponModFabric implements ModInitializer {
     public void onInitialize() {
         BalkonsWeaponMod.init();
         WMConfigConditionFabric.init();
+
+        try {
+            Class<?> clazz = Class.forName("dev.architectury.event.fabric.LootTableModificationContextImpl");
+            Constructor<?> constructor = clazz.getDeclaredConstructor(LootTable.Builder.class);
+            constructor.setAccessible(true);
+            MethodHandles.Lookup lookup = MethodHandles.lookup();
+            MethodHandle handle = lookup.unreflectConstructor(constructor);
+
+            LootTableEvents.MODIFY.register((key, tableBuilder, source, registries) -> {
+                try {
+                    WMCommonEventHandler.MODIFY_LOOT_TABLE.invoker().modifyLootTable(
+                            registries, key,
+                            (LootEvent.LootTableModificationContext) handle.invoke(tableBuilder),
+                            source.isBuiltin());
+                } catch (Throwable t) {
+                    BalkonsWeaponMod.LOGGER.error("Loot tables could not be modified!", t);
+                }
+            });
+        } catch (Throwable t) {
+            BalkonsWeaponMod.LOGGER.error("Loot tables could not be modified!", t);
+        }
     }
 
 }
