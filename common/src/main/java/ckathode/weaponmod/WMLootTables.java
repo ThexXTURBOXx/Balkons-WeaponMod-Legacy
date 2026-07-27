6b71a516260e719182db58438152a8870972ec51
Nico Mexis <nico.mexis@kabelmail.de>
2026-07-27 23:11:08 +0200

    Increase amount of explosives in loot tables

--- common/src/main/java/ckathode/weaponmod/WMLootTables.java
+++ common/src/main/java/ckathode/weaponmod/WMLootTables.java
@@ -358,10 +358,14 @@ public class WMLootTables {
         }
         if (BuiltInLootTables.BURIED_TREASURE.equals(id)) {
             context.addPool(getIronWeaponsLootPool().setRolls(RandomValueBounds.between(0, 1)));
-            context.addPool(getExplosiveLootPool().setRolls(RandomValueBounds.between(0, 1)));
+            context.addPool(getExplosiveLootPool()
+                    .apply(SetItemCountFunction.setCount(RandomValueBounds.between(4, 9)))
+                    .setRolls(RandomValueBounds.between(0, 1)));
         }
         if (BuiltInLootTables.DESERT_PYRAMID.equals(id)) {
-            context.addPool(getExplosiveLootPool().setRolls(RandomValueBounds.between(0, 1)));
+            context.addPool(getExplosiveLootPool()
+                    .apply(SetItemCountFunction.setCount(RandomValueBounds.between(4, 9)))
+                    .setRolls(RandomValueBounds.between(0, 1)));
         }
         if (BuiltInLootTables.END_CITY_TREASURE.equals(id)) {
             LootPool.Builder lootPool = LootPool.lootPool();
@@ -389,7 +393,9 @@ public class WMLootTables {
             context.addPool(lootPool.setRolls(RandomValueBounds.between(0, 1)));
         }
         if (BuiltInLootTables.SHIPWRECK_SUPPLY.equals(id)) {
-            context.addPool(getExplosiveLootPool().setRolls(RandomValueBounds.between(0, 1)));
+            context.addPool(getExplosiveLootPool()
+                    .apply(SetItemCountFunction.setCount(RandomValueBounds.between(4, 9)))
+                    .setRolls(RandomValueBounds.between(0, 1)));
         }
         if (BuiltInLootTables.STRONGHOLD_CORRIDOR.equals(id)) {
             context.addPool(getIronWeaponsLootPool().setRolls(RandomValueBounds.between(0, 1)));
